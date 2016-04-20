e7fda9aeb1a35bb3db6099fe5c74163c0af4ed31
yanzhuan051 <13317182430@163.com>
2016-04-20 12:53:55 +0800

    add

---
+++ resto-shop-web/src/main/java/com/resto/shop/web/controller/business/BrandSettingController.java
@@ -0,0 +1,49 @@
+ package com.resto.shop.web.controller.business;
+
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.resto.shop.web.controller.GenericController;
+import com.resto.brand.core.entity.Result;
+import com.resto.brand.web.model.Brand;
+import com.resto.brand.web.model.BrandSetting;
+import com.resto.brand.web.service.BrandService;
+import com.resto.brand.web.service.BrandSettingService;
+
+@Controller
+@RequestMapping("brandSetting")
+public class BrandSettingController extends GenericController{
+
+	@Resource
+	BrandSettingService brandSettingService;
+	
+	@Resource
+	BrandService brandService;
+	
+	@RequestMapping("/list")
+    public void list(){
+    }
+
+	@RequestMapping("list_one")
+	@ResponseBody
+	public Result list_one(){
+		Brand brand = brandService.selectById(getCurrentBrandId());
+		String brandSettingId = brand.getBrandSettingId();
+		BrandSetting brandSetting = brandSettingService.selectById(brandSettingId);
+		return getSuccessResult(brandSetting);
+	}
+
+
+	@RequestMapping("modify")
+	@ResponseBody
+	public Result modify(@Valid BrandSetting brandSetting){
+		brandSettingService.update(brandSetting);
+		return Result.getSuccess();
+	}
+	
+}
