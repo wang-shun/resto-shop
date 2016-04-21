f20f24d16c7252ceb9a6bd9bb21c0ed7d36f7e56
yanzhuan051 <13317182430@163.com>
2016-04-21 15:01:24 +0800

    add

---
+++ resto-shop-api/src/main/java/com/resto/shop/web/constant/DistributionType.java
@@ -0,0 +1,21 @@
+package com.resto.shop.web.constant;
+
+public class DistributionType {
+	public final static int RESTAURANT_MODE_ID = 1;  //堂吃
+	public final static int DELIVERY_MODE_ID = 2;    //自提外卖
+	public static final int TAKE_IT_SELF = 3;		//外带
+	
+	
+	public static String getModeText(int mid){
+		switch (mid) {
+		case RESTAURANT_MODE_ID:
+			return "堂吃";
+		case DELIVERY_MODE_ID:
+			return "自提外卖";
+		case TAKE_IT_SELF:
+			return "外带";
+		default:
+			return "未知";
+		}
+	}
+}
