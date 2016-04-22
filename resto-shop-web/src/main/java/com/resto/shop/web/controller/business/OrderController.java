2a166c78a8dd692d3a65b6dd27496cef921e95ad
Diamond <458293193@qq.com>
2016-04-22 09:43:43 +0800

    remove

--- resto-shop-web/src/main/java/com/resto/shop/web/controller/business/OrderController.java
+++ resto-shop-web/src/main/java/com/resto/shop/web/controller/business/OrderController.java
@@ -1,71 +1,12 @@
  package com.resto.shop.web.controller.business;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.annotation.Resource;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.resto.brand.core.entity.Result;
-import com.resto.brand.web.model.ShopDetail;
-import com.resto.brand.web.service.ShopDetailService;
 import com.resto.shop.web.controller.GenericController;
-import com.resto.shop.web.model.Order;
-import com.resto.shop.web.service.OrderService;
 
 @Controller
 @RequestMapping("order")
 public class OrderController extends GenericController{
 
-	@Resource
-	OrderService orderService;
-	
-	@Resource
-	ShopDetailService shopDetailService;
-	
-	@RequestMapping("list_push")
-	@ResponseBody
-	public Result list_push(Long lastTime){
-		List<Order> order = orderService.selectPushOrder(getCurrentShopId(),lastTime);
-		return getSuccessResult(order);
-	}
-	
-	@RequestMapping("list_ready")
-	@ResponseBody
-	public Result list_ready(Long lastTime){
-		List<Order> order= orderService.selectReadyOrder(getCurrentShopId(),lastTime);
-		return getSuccessResult(order);
-	}
-	
-	@RequestMapping("list_call")
-	@ResponseBody
-	public Result list_call(Long lastTime){
-		List<Order> order = orderService.selectCallOrder(getCurrentBrandId(),lastTime);
-		return getSuccessResult(order);
-	}
-	
-	@RequestMapping("callNumber")
-	@ResponseBody
-	public Result callOrder(String orderId){
-		orderService.callNumber(orderId);
-		return getSuccessResult();
-	}
-	
-	@RequestMapping("/printReceipt")
-	@ResponseBody
-	public Map<String,Object> printReceipt(String orderId){
-		String shopDetailId = getCurrentShopId();
-		return orderService.printReceipt(orderId,shopDetailId);
-	}
-	
-	
-	@RequestMapping("testKitchen")
-	@ResponseBody
-	public Result testKitchen(String orderId){
-		return getSuccessResult(orderService.kitchenTest(orderId));
-	}
 }
