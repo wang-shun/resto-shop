ffd3e0e69eed1d932c8a746700654fa5a1a0b4cf
asong <906529775@qq.com>
2016-05-19 01:14:49 +0800

    add

--- resto-shop-web/src/main/java/com/resto/shop/web/controller/business/SmsChargeOrderController.java
+++ resto-shop-web/src/main/java/com/resto/shop/web/controller/business/SmsChargeOrderController.java
@@ -75,8 +75,6 @@ public class SmsChargeOrderController extends GenericController {
 	
 	@RequestMapping("/alipayNotify")
 	public void alipayNotify(HttpServletRequest request,HttpServletResponse response) throws UnsupportedEncodingException{
-		smsChargeOrderService.checkSmsChargeOrder();
-		
 		//获取支付宝POST过来反馈信息
 		Map<String,String> params = new HashMap<String,String>();
 		Map requestParams = request.getParameterMap();
@@ -100,34 +98,22 @@ public class SmsChargeOrderController extends GenericController {
 		String trade_no = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"),"UTF-8");
 		//交易状态
 		String trade_status = new String(request.getParameter("trade_status").getBytes("ISO-8859-1"),"UTF-8");
+		//交易金额
+		String total_fee = new String(request.getParameter("total_fee").getBytes("ISO-8859-1"),"UTF-8");
+		//收款支付宝账号
+		String seller_id = new String(request.getParameter("seller_id").getBytes("ISO-8859-1"),"UTF-8");
 		//获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以上仅供参考)//
-			
 		//返回值
 		String returnHtml = "fail";
 		if(AlipayNotify.verify(params)){//验证成功
-			if(trade_status.equals("TRADE_FINISHED")){
-				//判断该笔订单是否在商户网站中已经做过处理
-					//如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-					//请务必判断请求时的total_fee、seller_id与通知时获取的total_fee、seller_id为一致的
-					//如果有做过处理，不执行商户的业务程序
-					
+			if(trade_status.equals("TRADE_FINISHED") || trade_status.equals("TRADE_SUCCESS")){
+				smsChargeOrderService.checkSmsChargeOrder(out_trade_no,trade_no,total_fee,seller_id);
 				//注意：
 				//退款日期超过可退款期限后（如三个月可退款），支付宝系统发送该交易状态通知
-			} else if (trade_status.equals("TRADE_SUCCESS")){
-				//判断该笔订单是否在商户网站中已经做过处理
-					//如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-					//请务必判断请求时的total_fee、seller_id与通知时获取的total_fee、seller_id为一致的
-					//如果有做过处理，不执行商户的业务程序
-					
-				//注意：
-				//付款完成后，支付宝系统发送该交易状态通知
 			}
-
-			//——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
-				
 			returnHtml = "success";	//请不要修改或删除
-
-			//////////////////////////////////////////////////////////////////////////////////////////
+		}else{//验证失败
+			returnHtml = "fail";
 		}
 		//返回
 		try {
