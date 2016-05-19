636517f2359809be4480a1e17f538d77e4eaca9d
asong <906529775@qq.com>
2016-05-20 02:54:03 +0800

    add

--- resto-shop-server/src/main/java/com/resto/shop/web/aspect/SmsAspect.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/aspect/SmsAspect.java
@@ -23,41 +23,41 @@ import com.resto.brand.web.service.SmsAcountService;
 @Aspect
 public class SmsAspect {
 
-    Logger log = LoggerFactory.getLogger(getClass());
-	
-	@Resource
-	private SmsAcountService smsAcountService;
-	
-	@Pointcut("execution(* com.resto.brand.core.util.sendMessage(..))")
-	public void sendCode(){};
-	
-	//发短信前看是否有剩余条数
-	@Before(value="sendMessage()")
-	public void sendMessageBefore(String phone,String data,String sign, String codeSmsTemp,String brandId){
-		//查询这个商家的剩余短信的条数
-		SmsAcount smsCount = smsAcountService.selectByBrandId(brandId);
-		if(smsCount.getRemainderNum()<=0){
-			//通知商家短信余额不足需要充值
-			System.out.println(",,");
-			//不能执行发短信的方法
-		    return;
-		}
-	}
-	
-	@AfterReturning(value="sendMessage()")
-	public void sendCodeAfter(String phone,String data,String sign, String codeSmsTemp,String brandId) throws Throwable{
-		//减少一条剩余短信的条数 和增加一条短信使用的条数
-	        smsAcountService.updateByBrandId(brandId);
-	        
-	        //判断当前剩余短信的条数是否需要提醒商家
-		SmsAcount smsAccount = smsAcountService.selectByBrandId(brandId);
-		
-		if(NoticeType.first==smsAccount.getRemainderNum()||NoticeType.second==smsAccount.getRemainderNum()||NoticeType.last==smsAccount.getRemainderNum()){
-		    //通知商家短信不足
-		    
-		}
-		
-		
-	}
+//    Logger log = LoggerFactory.getLogger(getClass());
+//	
+//	@Resource
+//	private SmsAcountService smsAcountService;
+//	
+//	@Pointcut("execution(* com.resto.brand.core.util.sendMessage(..))")
+//	public void sendCode(){};
+//	
+//	//发短信前看是否有剩余条数
+//	@Before(value="sendMessage()")
+//	public void sendMessageBefore(String phone,String data,String sign, String codeSmsTemp,String brandId){
+//		//查询这个商家的剩余短信的条数
+//		SmsAcount smsCount = smsAcountService.selectByBrandId(brandId);
+//		if(smsCount.getRemainderNum()<=0){
+//			//通知商家短信余额不足需要充值
+//			System.out.println(",,");
+//			//不能执行发短信的方法
+//		    return;
+//		}
+//	}
+//	
+//	@AfterReturning(value="sendMessage()")
+//	public void sendCodeAfter(String phone,String data,String sign, String codeSmsTemp,String brandId) throws Throwable{
+//		//减少一条剩余短信的条数 和增加一条短信使用的条数
+//	        smsAcountService.updateByBrandId(brandId);
+//	        
+//	        //判断当前剩余短信的条数是否需要提醒商家
+//		SmsAcount smsAccount = smsAcountService.selectByBrandId(brandId);
+//		
+//		if(NoticeType.first==smsAccount.getRemainderNum()||NoticeType.second==smsAccount.getRemainderNum()||NoticeType.last==smsAccount.getRemainderNum()){
+//		    //通知商家短信不足
+//		    
+//		}
+//		
+//		
+//	}
 	
 }
