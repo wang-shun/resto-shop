164871bc6cc499b2980cde2eb7f0c54db6aae0a7
Diamond <458293193@qq.com>
2016-05-24 17:52:01 +0800

    add clearshopcart

--- resto-shop-server/src/main/java/com/resto/shop/web/dao/ShopCartMapper.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/dao/ShopCartMapper.java
@@ -2,8 +2,6 @@ package com.resto.shop.web.dao;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
-
 import com.resto.brand.core.generic.GenericDao;
 import com.resto.shop.web.model.ShopCart;
 
@@ -29,5 +27,5 @@ public interface ShopCartMapper  extends GenericDao<ShopCart,Integer> {
 
 	void clearShopCart(String customerId, Integer distributionModeId, String shopDetailId);
 
-	void clearAllShopCart(@Param("customrId")String customerId, @Param("shopDetailId")String shopDetailId);
+	void clearAllShopCart(String customerId,String shopDetailId);
 }
