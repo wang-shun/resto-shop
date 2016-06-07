daecd1e786dbcb16e24a98de3efa7d31c742566e
yanzhuan051 <13317182430@163.com>
2016-06-07 18:15:25 +0800

    add

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/SmsLogServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/SmsLogServiceImpl.java
@@ -21,7 +21,6 @@ import com.resto.brand.web.service.BrandSettingService;
 import com.resto.brand.web.service.BrandUserService;
 import com.resto.brand.web.service.SmsAcountService;
 import com.resto.shop.web.constant.SmsLogType;
-import com.resto.shop.web.constant.SmsNumNotice;
 import com.resto.shop.web.dao.SmsLogMapper;
 import com.resto.shop.web.model.SmsLog;
 import com.resto.shop.web.service.SmsLogService;
@@ -65,6 +64,7 @@ public class SmsLogServiceImpl extends GenericServiceImpl<SmsLog, Long> implemen
 		//查询
 		BrandUser brandUser = brandUserService.selectById(b.getBrandUserId());
 		
+		//发送短信返回
 		String string = sendMsg(brandSetting.getSmsSign(), b.getBrandName(), code, phone,brandUser);
 		
 		SmsLog smsLog = new SmsLog();
@@ -76,55 +76,67 @@ public class SmsLogServiceImpl extends GenericServiceImpl<SmsLog, Long> implemen
 		smsLog.setPhone(phone);
 		smsLog.setSmsResult(string);
 		JSONObject obj = new JSONObject(string);
-		if(obj.optBoolean("success",false)){
+		
+		//返回值中有"success":"false"时说明商家无法发短信或者该条短信发送失败,此时不更新短信账户
+		if(obj.optBoolean("success", true)){
+			//
+			if(obj.getBoolean("success")){
+				try{
+					insert(smsLog);
+					//更新短信账户的信息
+					smsAcountService.updateByBrandId(brandId);
+					//判断是否要提醒商家充值短信账户
+					sendNotice(b,brandUser);
+				}catch(Exception e){
+					log.error("发送短信失败:"+e.getMessage());
+				}
+			}else{
+				//短信发送失败不更新短信账户
+				insert(smsLog);
+			}
 			
 		}
 		log.info("短信发送结果:"+string);
-		try{
-			insert(smsLog);
-			//更新短信账户的信息
-			smsAcountService.updateByBrandId(brandId);
-			//判断是否要提醒商家充值短信账户
-			sendNotice(b,brandUser);
-		}catch(Exception e){
-			log.error("发送短信失败:"+e.getMessage());
-		}
+		
+	
 		return string;
 	}
 
 	private void sendNotice(Brand b,BrandUser brandUser) {
 		SmsAcount smsAcount = smsAcountService.selectByBrandId(b.getId());
-		switch (smsAcount.getRemainderNum()) {
-		case SmsNumNotice.NOTICE_FIRST:
-			SMSUtils.sendNoticeToBrand("餐加", "餐加咨询管理", b.getBrandName(),SmsNumNotice.NOTICE_FIRST, brandUser.getPhone());
-			break;
-		case SmsNumNotice.NOTICE_SECOND:
-			SMSUtils.sendNoticeToBrand("餐加", "餐加咨询管理", b.getBrandName(),SmsNumNotice.NOTICE_SECOND, brandUser.getPhone());
-			break;
-		case SmsNumNotice.NOTICE_LAST:
-			SMSUtils.sendNoticeToBrand("餐加", "餐加咨询管理", b.getBrandName(),SmsNumNotice.NOTICE_LAST, brandUser.getPhone());
-			break;
-		default:
-			break;
+		//获取短信账户短信提醒
+		String str = smsAcount.getSmsRemind();
+		String[] arrs = str.split(",");
+		//获取商家短信剩余数量
+		int remindNum = smsAcount.getRemainderNum();
+		//判断是否需要提醒
+		if(this.isHave(arrs, remindNum+"")){
+			//提醒商家充值
+			SMSUtils.sendNoticeToBrand("餐加", "餐加咨询管理", b.getBrandName(),remindNum, brandUser.getPhone());
+			
 		}
-		
 	}
 
-	/**
-	 * 这个方法做增强
-	 */
+	
 	public String sendMsg(String sign,String serviceName,String code,String phone,BrandUser brandUser){
 		//判断该品牌账户的余额是否充足
 		SmsAcount smsAcount = smsAcountService.selectByBrandId(brandUser.getBrandId());
-		
-		if(SmsNumNotice.NOTICE_OWING_FIRST==smsAcount.getRemainderNum()){
-			SMSUtils.sendNoticeToBrand("餐加", "餐加咨询管理", serviceName,SmsNumNotice.NOTICE_OWING_FIRST, brandUser.getPhone());
-		}else if(SmsNumNotice.NOTICE_OWING_SECOND==smsAcount.getRemainderNum()){
-			SMSUtils.sendNoticeToBrand("餐加", "餐加咨询管理", serviceName,SmsNumNotice.NOTICE_OWING_SECOND, brandUser.getPhone());
-		}else if(SmsNumNotice.NOTICE_OWING_LAST > smsAcount.getRemainderNum()||SmsNumNotice.NOTICE_OWING_LAST==smsAcount.getRemainderNum()){
+		//获取剩余短信条数
+		int remindNum = smsAcount.getRemainderNum();
+		String [] arrs = smsAcount.getSmsRemind().split(",");
+		//判断剩余短信条数是否大于设定的最小可发短信值
+		if(remindNum<this.getMinStr(arrs)){
+			//我们提醒商家充值
 			SMSUtils.sendNoticeToBrand("餐加", "餐加咨询管理", serviceName,smsAcount.getRemainderNum(), brandUser.getPhone());
-			log.info("欠费超过50无法发短信");
-			return "{'msg':'当前品牌已欠费，请充值后使用短信功能','success':'false'}";
+			log.info("剩余短信为"+remindNum+"条无法发短信");
+			//返回false标记让商家无法发短信
+			return "{'msg':'当前品牌已超欠费可用额度，请充值后使用短信功能','success':'false'}";
+		}else{
+			//剩余短信在设置的范围内
+			if(this.isHave(arrs, remindNum+"")){
+				//发短信提醒商家
+				SMSUtils.sendNoticeToBrand("餐加", "餐加咨询管理", serviceName, remindNum, phone);
+			}
 		}
 		//商家给客户发短信
 		return SMSUtils.sendCode(sign, serviceName, code, phone);
@@ -165,4 +177,38 @@ public class SmsLogServiceImpl extends GenericServiceImpl<SmsLog, Long> implemen
 		}
 		return list;
 	}
+	
+	
+	/**
+	 * String数组获取最小值
+	 * @param arrs
+	 * @return
+	 */
+	
+	public int getMinStr(String[] arrs){
+		if(arrs == null ||arrs.length<=0){
+			 throw new IllegalArgumentException("空数组无法获取最小值");
+		}
+		int min = Integer.parseInt(arrs[0]);
+		for(int i=0;i<arrs.length;i++){
+			int temp = Integer.parseInt(arrs[i]);
+			if(min>temp){
+				min = temp;
+			}
+		}
+		return min;
+	}
+	
+	public  boolean isHave(String[] strs,String s){
+		  /*此方法有两个参数，第一个是要查找的字符串数组，第二个是要查找的字符或字符串
+		   * */
+		  for(int i=0;i<strs.length;i++){
+		   if(strs[i].indexOf(s)!=-1){//循环查找字符串数组中的每个字符串中是否包含所有查找的内容
+		    return true;//查找到了就返回真，不在继续查询
+		   }
+		  }
+		  return false;//没找到返回false
+		 }
+	
+	
 }
