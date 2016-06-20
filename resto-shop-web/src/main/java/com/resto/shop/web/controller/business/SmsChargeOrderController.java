8395ec32d7bbb8f40707745668e105efc8077a25
yanzhuan051 <13317182430@163.com>
2016-06-20 11:20:39 +0800

    add

--- resto-shop-web/src/main/java/com/resto/shop/web/controller/business/SmsChargeOrderController.java
+++ resto-shop-web/src/main/java/com/resto/shop/web/controller/business/SmsChargeOrderController.java
@@ -57,29 +57,37 @@ public class SmsChargeOrderController extends GenericController {
 	@RequestMapping("/smsCharge")
 	public void smsCharge(String chargeMoney,String paytype,HttpServletRequest request,HttpServletResponse response) throws IOException, WriterException, DocumentException{
 		String returnHtml = "<h1>参数错误！</h1>";
+//		String errorHtml="<h1>充值金额小于100无法充值<h1>";
 		if(StringUtil.isNotEmpty(chargeMoney) && StringUtil.isNotEmpty(paytype)){
-			SmsChargeOrder smsChargeOrder = smsChargeOrderService.saveSmsOrder(getCurrentBrandId(), chargeMoney);//创建充值订单
-			String out_trade_no = smsChargeOrder.getId();
-			if(paytype.equals(PayType.ALI_PAY+"")){//支付宝支付
-				String show_url = "";///商品展示页面
-				String notify_url = getBaseUrl()+"paynotify/alipay_notify";
-				String return_url = getBaseUrl()+"paynotify/alipay_return";
-				String subject = "短信充值";
-				Map<String, String> formParame = AlipaySubmit.createFormParame(out_trade_no, subject, chargeMoney, show_url, notify_url, return_url, null);
-				returnHtml = AlipaySubmit.buildRequest(formParame, "post", "确认");
-			}else if(paytype.equals(PayType.WECHAT_PAY+"")){//微信支付
-				String spbill_create_ip = InetAddress.getLocalHost().getHostAddress();
-				String notify_url =  getBaseUrl()+"paynotify/wxpay_notify";
-				log.info("微信的通知路径为："+notify_url);
-				String body = "短信充值";
-				Map<String,String> apiReqeust = WeChatPayUtils.createWxPay(out_trade_no, chargeMoney, spbill_create_ip, notify_url,body);
-				if("true".equals(apiReqeust.get("success"))){
-					request.getSession().setAttribute("wxPayCode", apiReqeust.get("url"));
-					returnHtml = getWxPayHtml();
+			//判断下单的金额是否大于100
+//			if(new BigDecimal(chargeMoney).compareTo(new BigDecimal("100"))==-1){
+//				log.info("充值金额不足100，无法充值");
+//				outprint(errorHtml, response);
+//			}
+//			else{
+				SmsChargeOrder smsChargeOrder = smsChargeOrderService.saveSmsOrder(getCurrentBrandId(), chargeMoney);//创建充值订单
+				String out_trade_no = smsChargeOrder.getId();
+				if(paytype.equals(PayType.ALI_PAY+"")){//支付宝支付
+					String show_url = "";///商品展示页面
+					String notify_url = getBaseUrl()+"paynotify/alipay_notify";
+					String return_url = getBaseUrl()+"paynotify/alipay_return";
+					String subject = "短信充值";
+					Map<String, String> formParame = AlipaySubmit.createFormParame(out_trade_no, subject, chargeMoney, show_url, notify_url, return_url, null);
+					returnHtml = AlipaySubmit.buildRequest(formParame, "post", "确认");
+				}else if(paytype.equals(PayType.WECHAT_PAY+"")){//微信支付
+					String spbill_create_ip = InetAddress.getLocalHost().getHostAddress();
+					String notify_url =  getBaseUrl()+"paynotify/wxpay_notify";
+					log.info("微信的通知路径为："+notify_url);
+					String body = "短信充值";
+					Map<String,String> apiReqeust = WeChatPayUtils.createWxPay(out_trade_no, chargeMoney, spbill_create_ip, notify_url,body);
+					if("true".equals(apiReqeust.get("success"))){
+						request.getSession().setAttribute("wxPayCode", apiReqeust.get("url"));
+						returnHtml = getWxPayHtml();
+					}
 				}
 			}
-		}
-		outprint(returnHtml, response);
+			outprint(returnHtml, response);
+		//}
 	}
 	
 	@RequestMapping("/smsChargeByBank")
