aa09ff5eed921e0569aa4fff8bc65b37938ea788
Diamond <458293193@qq.com>
2016-06-23 19:35:54 +0800

    add

--- resto-shop-server/src/main/java/com/resto/shop/web/aspect/OrderAspect.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/aspect/OrderAspect.java
@@ -174,7 +174,7 @@ public class OrderAspect {
 		if(order.getAllowAppraise()){
 			StringBuffer msg = new StringBuffer();
 			msg.append("您有一个红包未领取\n");
-			msg.append("<a href='"+setting.getWechatWelcomeUrl()+"?subpage=my&&dialog=redpackage&&orderId="+order.getId()+"'>点击领取</a>");
+			msg.append("<a href='"+setting.getWechatWelcomeUrl()+"?subpage=my&dialog=redpackage&orderId="+order.getId()+"'>点击领取</a>");
 			
 			String result = WeChatUtils.sendCustomerMsg(msg.toString(), customer.getWechatId(), config.getAppid(), config.getAppsecret());
 			log.info("发送评论通知成功:"+msg+result);
@@ -205,7 +205,7 @@ public class OrderAspect {
 	private void sendRewardShareMsg(Customer customer, WechatConfig config, BrandSetting setting, BigDecimal rewardMoney) {
 		StringBuffer msg = new StringBuffer();
 		msg.append("感谢您的分享，您的好友已在餐厅消费，我们赠送了 "+rewardMoney.setScale(2, BigDecimal.ROUND_HALF_UP)+" 元的红包到您的账户\n");
-		msg.append("<a href='"+setting.getWechatWelcomeUrl()+"?subpage=my&&dialog=account'>点击查看余额</a>");
+		msg.append("<a href='"+setting.getWechatWelcomeUrl()+"?subpage=my&dialog=account'>点击查看余额</a>");
 		String result = WeChatUtils.sendCustomerMsg(msg.toString(), customer.getWechatId(), config.getAppid(), config.getAppsecret());
 		log.info("发送返利通知成功:"+customer.getId()+" MSG: "+msg+result);
 	}
