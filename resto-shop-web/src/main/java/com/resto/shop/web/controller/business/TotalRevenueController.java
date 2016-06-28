2ad1be0708d3bfd8cbd101b3334684609872ba68
yanzhuan051 <13317182430@163.com>
2016-06-28 16:03:06 +0800

    add

--- resto-shop-web/src/main/java/com/resto/shop/web/controller/business/TotalRevenueController.java
+++ resto-shop-web/src/main/java/com/resto/shop/web/controller/business/TotalRevenueController.java
@@ -1,31 +1,31 @@
  package com.resto.shop.web.controller.business;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFCellStyle;
-import org.apache.poi.hssf.usermodel.HSSFDataFormat;
-import org.apache.poi.hssf.usermodel.HSSFFont;
-import org.apache.poi.hssf.usermodel.HSSFHyperlink;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.util.CellRangeAddress;
-import org.apache.poi.hssf.util.HSSFColor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.resto.brand.core.util.ExcelUtil;
 import com.resto.brand.web.dto.BrandIncomeDto;
 import com.resto.brand.web.dto.IncomeReportDto;
 import com.resto.brand.web.dto.ShopIncomeDto;
@@ -53,172 +53,177 @@ public class TotalRevenueController extends GenericController{
 	@RequestMapping("/list")
         public void list(){
         }
+	
 	//封装品牌和店铺收入需要的数据
+	public Map<String,Object> getIncomeReportList(String beginDate,String endDate){
+		//查询品牌和店铺的收入情况
+				List<IncomeReportDto> incomeReportList = orderpaymentitemService.selectIncomeList(getCurrentBrandId(),beginDate,endDate);
+				//封装店铺所需要的数据结构
+				List<ShopDetail> listShop = shopDetailService.selectByBrandId(getCurrentBrandId());
+				List<ShopIncomeDto> shopIncomeList = new ArrayList<>();
+				Map<String,ShopIncomeDto> hm = new HashMap<>();
+				for (int i = 0; i < listShop.size(); i++) {//实际有多少个店铺显示多少个数据
+					ShopIncomeDto sin = new ShopIncomeDto();
+					sin.setShopDetailId(listShop.get(i).getId());
+					sin.setShopName(listShop.get(i).getName());
+					//设置每个店铺初始营业额为零
+					BigDecimal temp = BigDecimal.ZERO;
+					sin.setWechatIncome(temp);
+					sin.setRedIncome(temp);
+					sin.setCouponIncome(temp);
+					sin.setChargeAccountIncome(temp);
+					sin.setChargeGifAccountIncome(temp);
+					sin.setTotalIncome(temp, temp, temp,temp,temp);
+					String s = ""+i;
+					hm.put(s, sin);
+					if(!incomeReportList.isEmpty()){
+						for(IncomeReportDto in : incomeReportList){
+					        if(hm.get(s).getShopDetailId().equals(in.getShopDetailId())){
+					            switch (in.getPayMentModeId()) {
+								case PayMode.WEIXIN_PAY:
+									hm.get(s).setWechatIncome(in.getPayValue());
+									break;
+								case PayMode.ACCOUNT_PAY:
+									hm.get(s).setRedIncome(in.getPayValue());
+									break;
+								case PayMode.COUPON_PAY:
+									hm.get(s).setCouponIncome(in.getPayValue());
+									break;
+								case PayMode.CHARGE_PAY:
+									hm.get(s).setChargeAccountIncome(in.getPayValue());
+									break;
+								case PayMode.REWARD_PAY:
+									hm.get(s).setChargeGifAccountIncome(in.getPayValue());
+									break;
+									
+								default:
+									break;
+								}
+					            hm.get(s).setTotalIncome(hm.get(s).getWechatIncome(),hm.get(s).getRedIncome(),hm.get(s).getCouponIncome(),hm.get(s).getChargeAccountIncome(),hm.get(s).getChargeGifAccountIncome());
+					        }
+						}
+					}
+					shopIncomeList.add(hm.get(s));
+				}
+				//封装brand所需要的数据结构
+				
+				Brand brand = brandService.selectById(getCurrentBrandId());
+				List<BrandIncomeDto> brandIncomeList = new ArrayList<>();
+				BrandIncomeDto in = new BrandIncomeDto();
+				//初始化品牌的信息
+				BigDecimal wechatIncome = BigDecimal.ZERO;
+				BigDecimal redIncome = BigDecimal.ZERO;
+				BigDecimal couponIncome = BigDecimal.ZERO;
+				BigDecimal chargeAccountIncome = BigDecimal.ZERO;
+				BigDecimal chargeGifAccountIncome = BigDecimal.ZERO;
+				
+				if(!incomeReportList.isEmpty()){
+					for(IncomeReportDto income : incomeReportList){
+						if(income.getPaymentModeId()==PayMode.WEIXIN_PAY){
+							wechatIncome=wechatIncome.add(income.getPayValue()).setScale(2);
+						}else if(income.getPayMentModeId()==PayMode.ACCOUNT_PAY){
+							redIncome=redIncome.add(income.getPayValue()).setScale(2);
+						}else if(income.getPayMentModeId()==PayMode.COUPON_PAY){
+							couponIncome=couponIncome.add(income.getPayValue()).setScale(2);
+						}else if(income.getPaymentModeId()==PayMode.CHARGE_PAY){
+							chargeAccountIncome=chargeAccountIncome.add(income.getPayValue()).setScale(2);
+						}else if (income.getPayMentModeId()==PayMode.REWARD_PAY){
+							chargeGifAccountIncome=chargeGifAccountIncome.add(income.getPayValue()).setScale(2);
+						}
+					}
+				}
+				in.setBrandName(brand.getBrandName());
+				in.setWechatIncome(wechatIncome);
+				in.setRedIncome(redIncome);
+				in.setCouponIncome(couponIncome);
+				in.setChargeAccountIncome(chargeAccountIncome);
+				in.setChargeGifAccountIncome(chargeGifAccountIncome);
+				in.setTotalIncome(in.getWechatIncome(), in.getRedIncome(), in.getCouponIncome(),in.getChargeAccountIncome(),in.getChargeGifAccountIncome());
+				brandIncomeList.add(in);
+				Map<String,Object> map = new HashMap<>();
+				map.put("shopIncome", shopIncomeList);
+				map.put("brandIncome", brandIncomeList);
+				return map;
+	}
 	
 	@RequestMapping("reportIncome")
 	@ResponseBody
 	public Map<String,Object> selectIncomeReportList(@RequestParam("beginDate")String beginDate,@RequestParam("endDate")String endDate){
-		//查询品牌和店铺的收入情况
-		List<IncomeReportDto> incomeReportList = orderpaymentitemService.selectIncomeList(getCurrentBrandId(),beginDate,endDate);
-		//封装店铺所需要的数据结构
-		List<ShopDetail> listShop = shopDetailService.selectByBrandId(getCurrentBrandId());
-		List<ShopIncomeDto> shopIncomeList = new ArrayList<>();
-		Map<String,ShopIncomeDto> hm = new HashMap<>();
-		for (int i = 0; i < listShop.size(); i++) {//实际有多少个店铺显示多少个数据
-			ShopIncomeDto sin = new ShopIncomeDto();
-			sin.setShopDetailId(listShop.get(i).getId());
-			sin.setShopName(listShop.get(i).getName());
-			//设置每个店铺初始营业额为零
-			BigDecimal temp = BigDecimal.ZERO;
-			sin.setWechatIncome(temp);
-			sin.setRedIncome(temp);
-			sin.setCouponIncome(temp);
-			sin.setChargeAccountIncome(temp);
-			sin.setChargeGifAccountIncome(temp);
-			sin.setTotalIncome(temp, temp, temp,temp,temp);
-			String s = ""+i;
-			hm.put(s, sin);
-			if(!incomeReportList.isEmpty()){
-				for(IncomeReportDto in : incomeReportList){
-			        if(hm.get(s).getShopDetailId().equals(in.getShopDetailId())){
-			            switch (in.getPayMentModeId()) {
-						case PayMode.WEIXIN_PAY:
-							hm.get(s).setWechatIncome(in.getPayValue());
-							break;
-						case PayMode.ACCOUNT_PAY:
-							hm.get(s).setRedIncome(in.getPayValue());
-							break;
-						case PayMode.COUPON_PAY:
-							hm.get(s).setCouponIncome(in.getPayValue());
-							break;
-						case PayMode.CHARGE_PAY:
-							hm.get(s).setChargeAccountIncome(in.getPayValue());
-							break;
-						case PayMode.REWARD_PAY:
-							hm.get(s).setChargeGifAccountIncome(in.getPayValue());
-							break;
-							
-						default:
-							break;
-						}
-			            hm.get(s).setTotalIncome(hm.get(s).getWechatIncome(),hm.get(s).getRedIncome(),hm.get(s).getCouponIncome(),hm.get(s).getChargeAccountIncome(),hm.get(s).getChargeGifAccountIncome());
-			        }
-				}
-			}
-			shopIncomeList.add(hm.get(s));
-		}
-		//封装brand所需要的数据结构
 		
-		Brand brand = brandService.selectById(getCurrentBrandId());
-		List<BrandIncomeDto> brandIncomeList = new ArrayList<>();
-		BrandIncomeDto in = new BrandIncomeDto();
-		//初始化品牌的信息
-		BigDecimal wechatIncome = BigDecimal.ZERO;
-		BigDecimal redIncome = BigDecimal.ZERO;
-		BigDecimal couponIncome = BigDecimal.ZERO;
-		BigDecimal chargeAccountIncome = BigDecimal.ZERO;
-		BigDecimal chargeGifAccountIncome = BigDecimal.ZERO;
-		
-		if(!incomeReportList.isEmpty()){
-			for(IncomeReportDto income : incomeReportList){
-				if(income.getPaymentModeId()==PayMode.WEIXIN_PAY){
-					wechatIncome=wechatIncome.add(income.getPayValue()).setScale(2);
-				}else if(income.getPayMentModeId()==PayMode.ACCOUNT_PAY){
-					redIncome=redIncome.add(income.getPayValue()).setScale(2);
-				}else if(income.getPayMentModeId()==PayMode.COUPON_PAY){
-					couponIncome=couponIncome.add(income.getPayValue()).setScale(2);
-				}else if(income.getPaymentModeId()==PayMode.CHARGE_PAY){
-					chargeAccountIncome=chargeAccountIncome.add(income.getPayValue()).setScale(2);
-				}else if (income.getPayMentModeId()==PayMode.REWARD_PAY){
-					chargeGifAccountIncome=chargeGifAccountIncome.add(income.getPayValue()).setScale(2);
-				}
-			}
-		}
-		in.setBrandName(brand.getBrandName());
-		in.setWechatIncome(wechatIncome);
-		in.setRedIncome(redIncome);
-		in.setCouponIncome(couponIncome);
-		in.setChargeAccountIncome(chargeAccountIncome);
-		in.setChargeGifAccountIncome(chargeGifAccountIncome);
-		in.setTotalIncome(in.getWechatIncome(), in.getRedIncome(), in.getCouponIncome(),in.getChargeAccountIncome(),in.getChargeGifAccountIncome());
-		brandIncomeList.add(in);
-		Map<String,Object> map = new HashMap<>();
-		map.put("shopIncome", shopIncomeList);
-		map.put("brandIncome", brandIncomeList);
-		return map;
+		return getIncomeReportList(beginDate, endDate);
 	}
 	
-	@SuppressWarnings("deprecation")
+	/**
+	 * 品牌数据导出excel
+	 * @param beginDate
+	 * @param endDate
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value="/brandExprotExcel")
 	@ResponseBody
-	@RequestMapping("reportExcel")
-	public void reportExcel() throws IOException{
+	 public  void exprotBrandExcel(@RequestParam("beginDate")String beginDate,@RequestParam("endDate")String endDate,HttpServletRequest request, HttpServletResponse response) throws IOException, Exception{
+		 //导出文件名
+		  String str = "brandInCome.xls";
+		  String path = request.getSession().getServletContext().getRealPath(str);
+		  
+	  	  String[][] headers ={ { "品牌", "12" }, { "营收总额", "15" }, { "红包支付", "12" }, { "优惠券支付", "12" }, { "微信支付", "12" },{ "充值账户支付", "15" },{ "充值赠送账户支付", "19" }};
+		  String[]columns={ "brandName", "totalIncome", "redIncome", "couponIncome", "wechatIncome", "chargeAccountIncome", "chargeGifAccountIncome"};
+		  List<BrandIncomeDto> result=(List<BrandIncomeDto>) getIncomeReportList(beginDate, endDate).get("brandIncome");
+		  ExcelUtil<BrandIncomeDto> excelUtil=new ExcelUtil<BrandIncomeDto>();
+		  excelUtil.download(path, response);
+		  try{
+				OutputStream out = new FileOutputStream(path);
+				excelUtil.ExportExcel("品牌收入报表", headers, columns, result, out, "");
+				out.close();
+				JOptionPane.showMessageDialog(null, "导出成功！");
+				log.info("excel导出成功");
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+	}
+	
+	/**
+	 * 店铺数据导出excel
+	 * @param beginDate
+	 * @param endDate
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value="/shopExprotExcel")
+	@ResponseBody
+	public  void exprotShopExcel(@RequestParam("beginDate")String beginDate,@RequestParam("endDate")String endDate,HttpServletRequest request, HttpServletResponse response) throws IOException, Exception{
+		//导出文件名
+		String str = "shopInCome.xls";
+		String path = request.getSession().getServletContext().getRealPath(str);
 		
-		//品牌数据
-		BrandIncomeDto brandIncomeDto = new BrandIncomeDto(new BigDecimal("100"), new BigDecimal("50"), new BigDecimal("100"), new BigDecimal("100"), new BigDecimal("100"), new BigDecimal("100"), null, null, null, "测试品牌");
-		
-		//店铺数据
-		List<ShopIncomeDto> list = new ArrayList<>();
-		ShopIncomeDto shopIncomeDto = new ShopIncomeDto(new BigDecimal("100"), new BigDecimal("100"), new BigDecimal("100"), new BigDecimal("100"), new BigDecimal("100"), new BigDecimal("100"), null, "测试店铺1", null, null, null);
-		ShopIncomeDto shopIncomeDto2 = new ShopIncomeDto(new BigDecimal("100"), new BigDecimal("100"), new BigDecimal("100"), new BigDecimal("100"), new BigDecimal("100"), new BigDecimal("100"), null, "测试店铺2", null, null, null);
-		list.add(shopIncomeDto);
-		list.add(shopIncomeDto2);
-		
-		
-		//第一创建一个webbook,对应一个Excel文件
-		HSSFWorkbook wb = new HSSFWorkbook();
-		//第二步，在webbook中添加一个sheet,对应Excel文件中的sheet
-		HSSFSheet sheet = wb.createSheet("品牌表");
-		//第三步，在sheet中添加表头第0行
-		HSSFRow row = sheet.createRow(0);
-		//第四步，创建单元格,并设置值表头,设置表头居中
-		HSSFCellStyle style = wb.createCellStyle();
-		style.setAlignment(HSSFCellStyle.ALIGN_CENTER);//创建一个居中格式
-		
-		HSSFCell cell = row.createCell(0);
-		cell.setCellValue("品牌名称");
-		cell.setCellStyle(style);
-		
-		cell = row.createCell(1);
-		cell.setCellValue("总收入");
-		cell.setCellStyle(style);
-		
-		cell = row.createCell(2);
-		cell.setCellValue("红包收入");
-		cell.setCellStyle(style);
-		
-		cell = row.createCell(3);
-		cell.setCellValue("系统账户收入");
-		cell.setCellStyle(style);
-		
-		cell = row.createCell(4);
-		cell.setCellValue("优惠券收入");
-		cell.setCellStyle(style);
-		
-		cell = row.createCell(5);
-		cell.setCellValue("充值金额支付");
-		cell.setCellStyle(style);
-		
-		cell = row.createCell(6);
-		cell.setCellValue("充值赠送的金额支付");
-		cell.setCellStyle(style);
-		
-		//第五步写入实体数据
-		row = sheet.createRow(1);
-		row.createCell(0).setCellValue(brandIncomeDto.getBrandName());
-		row.createCell(0).setCellValue(brandIncomeDto.getBrandName());
-		row.createCell(0).setCellValue(brandIncomeDto.getBrandName());
-		row.createCell(0).setCellValue(brandIncomeDto.getBrandName());
-		
-	    
-	    
-	    
-	    
-	  
-	}  
+		String[][] headers ={ { "店铺", "12" }, { "营收总额", "15" }, { "红包支付", "12" }, { "优惠券支付", "12" }, { "微信支付", "12" },{ "充值账户支付", "15" },{ "充值赠送账户支付", "19" }};
+		String[]columns={ "shopName", "totalIncome", "redIncome", "couponIncome", "wechatIncome", "chargeAccountIncome", "chargeGifAccountIncome"};
+		List<ShopIncomeDto> result=(List<ShopIncomeDto>) getIncomeReportList(beginDate, endDate).get("shopIncome");
+		ExcelUtil<ShopIncomeDto> excelUtil=new ExcelUtil<ShopIncomeDto>();
+		excelUtil.download(path, response);
+		try{
+			OutputStream out = new FileOutputStream(path);
+			excelUtil.ExportExcel("店铺收入报表", headers, columns, result, out, "");
+			out.close();
+			JOptionPane.showMessageDialog(null, "导出成功！");
+			log.info("excel导出成功");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	
 	
 }
 	
-	
+
 	
 	
 
