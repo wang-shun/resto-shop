b3fcc8e19b4396b868ab3e3eb0f8deb5b3bd476c
luyujie <125823888@qq.com>
2016-07-29 18:32:36 +0800

    update

--- resto-shop-web/src/main/java/com/resto/shop/web/task/ShopTask.java
+++ resto-shop-web/src/main/java/com/resto/shop/web/task/ShopTask.java
@@ -1,91 +1,81 @@
 package com.resto.shop.web.task;
 
-import com.resto.brand.core.util.ApplicationUtils;
-import com.resto.brand.web.dto.ArticleSellDto;
 import com.resto.shop.web.controller.GenericController;
-import com.resto.shop.web.service.OrderService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.sql.*;
-import java.util.List;
 
 /**
  * Created by KONATA on 2016/7/14.
  */
 @Component("shopTask")
 public class ShopTask extends GenericController {
-	
-	@Autowired
-	private OrderService orderService;
- 
-	@Scheduled(cron = "00 56 14 * * ?")   //每天12点执行
-    public void job1() throws ClassNotFoundException {
-        String url = null;
-        String driver = null;
-        String username = null;
-        String password = null;
-        Connection con = null;
-        PreparedStatement sta = null;
 
-            try {
-                
-                url = "jdbc:mysql://127.0.0.1:3306/middle?useUnicode=true&characterEncoding=utf8";
-                driver = "com.mysql.jdbc.Driver";
-                Class.forName(driver);
-                username = "root";
-                password = "root";
-                con = DriverManager.getConnection(url, username, password);
+//	@Autowired
+//	private OrderService orderService;
+//
+//	@Scheduled(cron = "00 56 14 * * ?")   //每天12点执行
+//    public void job1() throws ClassNotFoundException {
+//        String url = null;
+//        String driver = null;
+//        String username = null;
+//        String password = null;
+//        Connection con = null;
+//        PreparedStatement sta = null;
+//
+//            try {
+//
+//                url = "jdbc:mysql://127.0.0.1:3306/middle?useUnicode=true&characterEncoding=utf8";
+//                driver = "com.mysql.jdbc.Driver";
+//                Class.forName(driver);
+//                username = "root";
+//                password = "root";
+//                con = DriverManager.getConnection(url, username, password);
+//
+//                String sql = "insert into shop_article(id,article_id,article_family_name,article_name,sales,sales_occupies,sell,sell_occupies) values(?,?,?,?,?,?,?,?)";
+// 			    sta = con.prepareStatement(sql);
+//
+// 			    //查询出店铺菜品的数据(今天的数据 )
+// 			    List<ArticleSellDto> list = orderService.selectShopArticleSellByDate("2016-07-20", "2016-07-20","f48a0a35e0be4dd8aaeb7cf727603958" ,"0asc");
+//
+// 			    for (ArticleSellDto articleSellDto : list) {
+// 			    	 sta.setString(1,ApplicationUtils.randomUUID());
+// 	 				 sta.setString(2, articleSellDto.getArticleId());
+// 	 				 sta.setString(3, articleSellDto.getArticleFamilyName());
+// 	 				 sta.setString(4, articleSellDto.getArticleName());
+// 	 				 sta.setInt(5, articleSellDto.getShopSellNum());
+// 	 				 sta.setString(6, articleSellDto.getNumRatio());
+// 	 				 sta.setBigDecimal(7, articleSellDto.getSalles());
+// 	 				 sta.setString(8, articleSellDto.getSalesRatio());
+// 	 				 sta.executeUpdate();
+//				}
+// 				 System.out.println("插入成功");
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            } finally {
+//                close(con, sta, null);
+//            }
+//
+//    }
+//
+//
+//    // 关闭相关的对象
+//    public void close(Connection con, PreparedStatement st, ResultSet rs) {
+//        try {
+//            if (rs != null) {
+//                rs.close();
+//            }
+//            if (st != null) {
+//                st.close();
+//            }
+//            if (con != null) {
+//                con.close();
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
 
-                String sql = "insert into shop_article(id,article_id,article_family_name,article_name,sales,sales_occupies,sell,sell_occupies) values(?,?,?,?,?,?,?,?)"; 
- 			    sta = con.prepareStatement(sql); 
- 			    
- 			    //查询出店铺菜品的数据(今天的数据 )
- 			    List<ArticleSellDto> list = orderService.selectShopArticleSellByDate("2016-07-20", "2016-07-20","f48a0a35e0be4dd8aaeb7cf727603958" ,"0asc");
- 			    
- 			    for (ArticleSellDto articleSellDto : list) {
- 			    	 sta.setString(1,ApplicationUtils.randomUUID()); 
- 	 				 sta.setString(2, articleSellDto.getArticleId()); 
- 	 				 sta.setString(3, articleSellDto.getArticleFamilyName());
- 	 				 sta.setString(4, articleSellDto.getArticleName());
- 	 				 sta.setInt(5, articleSellDto.getShopSellNum());
- 	 				 sta.setString(6, articleSellDto.getNumRatio());
- 	 				 sta.setBigDecimal(7, articleSellDto.getSalles());
- 	 				 sta.setString(8, articleSellDto.getSalesRatio());
- 	 				 sta.executeUpdate(); 
-				}
- 				 System.out.println("插入成功");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } finally {
-                close(con, sta, null);
-            }
-        
-    }
 
-
-    // 关闭相关的对象
-    public void close(Connection con, PreparedStatement st, ResultSet rs) {
-        try {
-            if (rs != null) {
-                rs.close();
-            }
-            if (st != null) {
-                st.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    
-    
-    
 //    public static void main(String[] args) throws ClassNotFoundException {
 //    	String url = "jdbc:mysql://127.0.0.1:3306/middle?useUnicode=true&characterEncoding=utf8";
 //    	String driver = "com.mysql.jdbc.Driver";
@@ -123,6 +113,6 @@ public class ShopTask extends GenericController {
 //	}
 //    
 //    
-    
-    
+
+
 }
