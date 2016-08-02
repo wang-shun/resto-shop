4ea76b9c39885dba13921cdc016b6b544b430a53
luyujie <125823888@qq.com>
2016-08-02 17:13:23 +0800

    update

--- resto-shop-server/src/main/java/com/resto/shop/web/dao/ArticleMapper.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/dao/ArticleMapper.java
@@ -63,4 +63,6 @@ public interface ArticleMapper extends GenericDao<Article, String>{
 	void initSizeCurrent();
 
 	void clearMain();
+
+	void initEmpty();
 }
--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/ArticleServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/ArticleServiceImpl.java
@@ -204,6 +204,7 @@ public class ArticleServiceImpl extends GenericServiceImpl<Article, String> impl
         articleMapper.editPriceStock(articleId,count);
         orderMapper.setStockBySuit();
         articleMapper.initSizeCurrent();
+        articleMapper.initEmpty();
         return true;
     }
 }
