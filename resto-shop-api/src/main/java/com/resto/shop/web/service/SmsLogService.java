0b72611c233454ef9af21f802caa1ad6d3e84937
luyujie <125823888@qq.com>
2016-08-22 20:01:48 +0800

    update

--- resto-shop-api/src/main/java/com/resto/shop/web/service/SmsLogService.java
+++ resto-shop-api/src/main/java/com/resto/shop/web/service/SmsLogService.java
@@ -27,6 +27,7 @@ public interface SmsLogService extends GenericService<SmsLog, Long> {
 	 * @return
 	 */
 	List<SmsLog> selectListWhere(String begin, String end,String shopIds);
+
 	/**
 	 * 根据品牌
 	 * 查询全部短信记录
