6fb8df1ed20b0e4c43c46901e15a66b9b563d4c7
luyujie <125823888@qq.com>
2016-08-25 10:42:59 +0800

    update

--- resto-shop-web/src/main/java/com/resto/shop/web/controller/business/ArticleController.java
+++ resto-shop-web/src/main/java/com/resto/shop/web/controller/business/ArticleController.java
@@ -69,49 +69,14 @@ public class ArticleController extends GenericController{
 	public Result create(@Valid @RequestBody Article article){
 		article.setShopDetailId(getCurrentShopId());
 		article.setUpdateUserId(getCurrentUserId());
-		article.setUpdateTime(new Date());	
+		article.setUpdateTime(new Date());
 		if(StringUtils.isEmpty(article.getId())){
 			article.setCreateUserId(getCurrentUserId());
 			articleService.save(article);
 		}else{
-			List<ArticlePrice> list = articlePriceService.selectByArticleId(article.getId());
-			if(article.getIsEmpty() == true){
-				if(article.getArticleType() == 1 && list.size() ==0){
-					article.setCurrentWorkingStock(0);
-					articleService.update(article);
-				} else if(article.getArticleType() == 1 && list.size() !=0){
-					article.setCurrentWorkingStock(0);
-					articleService.update(article);
-					for(ArticlePrice ap :list){
-						ap.setCurrentWorkingStock(0);
-						articlePriceService.update(ap);
-					}
-				}
-			}else {
-				if(article.getArticleType() == 1 && list.size() ==0) {
-					if (IsFreeday(new Date())) {
-						article.setCurrentWorkingStock(article.getStockWorkingDay());
-						articleService.update(article);
-					} else {
-						article.setCurrentWorkingStock(article.getStockWeekend());
-						articleService.update(article);
-					}
-				} else if (article.getArticleType() == 1 && list.size() !=0){
-					if (IsFreeday(new Date())) {
-						for(ArticlePrice ap : list){
-							ap.setCurrentWorkingStock(ap.getStockWorkingDay());
-							articlePriceService.update(ap);
-						}
-					} else {
-						for(ArticlePrice ap : list){
-							ap.setCurrentWorkingStock(ap.getStockWeekend());
-							articlePriceService.update(ap);
-						}
-					}
-				}
-			}
+			articleService.update(article);
 		}
-        articleService.initStock();
+		articleService.initStock();
 		return Result.getSuccess();
 	}
 	
