64e6fc797f12f20bc7b201269c3e4f742687d466
645534731 <645534731@qq.com>
2016-10-12 13:30:22 +0800

    王寅杰

--- resto-shop-server/src/main/java/com/resto/shop/web/aspect/OrderAspect.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/aspect/OrderAspect.java
@@ -230,9 +230,6 @@ public class OrderAspect {
                         MQMessageProducer.sendAutoConfirmOrder(order, setting.getAutoConfirmTime() * 1000);
                         MQMessageProducer.sendModelFivePaySuccess(order);
                     }
-
-
-
                 }
                 if (order.getOrderMode() != null) {
                     switch (order.getOrderMode()) {
