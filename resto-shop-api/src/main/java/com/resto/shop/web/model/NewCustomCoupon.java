fe58d48f043268d996ae5929a89794a228a93437
luyujie <125823888@qq.com>
2016-10-24 19:31:55 +0800

    update

--- resto-shop-api/src/main/java/com/resto/shop/web/model/NewCustomCoupon.java
+++ resto-shop-api/src/main/java/com/resto/shop/web/model/NewCustomCoupon.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+import com.resto.brand.web.model.ShopDetail;
 import org.springframework.format.annotation.DateTimeFormat;
 
 public class NewCustomCoupon {
@@ -17,9 +18,9 @@ public class NewCustomCoupon {
     @NotNull(message="优惠券的价值不能为空")
     @Min(message="优惠券的价值最小值0" ,value=0)
     private BigDecimal couponValue;//优惠券的价值
-    
+
     private Integer couponValiday;
-    
+
     @NotNull(message="优惠券的个数不能为空")
     private Integer couponNumber;
 
@@ -28,58 +29,43 @@ public class NewCustomCoupon {
     private Boolean useWithAccount;
 
     private String couponName;
-    
+
     @NotNull(message="优惠券的最低消费额度不能为空")
     @Min(message="最低消费额度为0",value=0)
     private BigDecimal couponMinMoney;
-    
+
     @DateTimeFormat(pattern=("HH:mm"))
     private Date beginTime;
-    
+
     @DateTimeFormat(pattern=("HH:mm"))
     private Date endTime;
-    
+
     @NotNull(message="是否启用不能为空")
     private Boolean isActivty;
 
     private String brandId;
-    
+
     private Integer couponType;//优惠券类型(-1:通用,0:新用户注册,1:邀请注册)
-    
+
     @NotNull(message="配送模式不能为空")
     private Integer distributionModeId;
-    
+
     /**
      * 新增字段用来区别优惠券使用时间的类型
      * 1.按天算 2.按范围算
      */
     @NotNull(message="优惠券类型不能为空")
     private Integer timeConsType;
-    
-    
-    
+
+
+
     @DateTimeFormat(pattern=("yyyy-MM-dd HH:mm:ss"))
     private Date beginDateTime;
-    
+
     @DateTimeFormat(pattern=("yyyy-MM-dd HH:mm:ss"))
     private Date endDateTime;
-    
-    public Integer getTimeConsType() {
-		return timeConsType;
-	}
 
-
-	private String shopDetailId;//新增字段店铺id 如果有表示是店铺的优惠券
-
-    private  Integer isBrand;//新增字段 是否是品牌专有
-
-    public Integer getIsBrand() {
-        return isBrand;
-    }
-
-    public void setIsBrand(Integer isBrand) {
-        this.isBrand = isBrand;
-    }
+    private String shopDetailId;
 
     public String getShopDetailId() {
         return shopDetailId;
@@ -89,27 +75,41 @@ public class NewCustomCoupon {
         this.shopDetailId = shopDetailId;
     }
 
+    private  Integer isBrand;
+
+    public Boolean getActivty() {
+        return isActivty;
+    }
+
+    public void setActivty(Boolean activty) {
+        isActivty = activty;
+    }
+
+    public Integer getTimeConsType() {
+        return timeConsType;
+    }
+
     public void setTimeConsType(Integer timeConsType) {
-		this.timeConsType = timeConsType;
-	}
+        this.timeConsType = timeConsType;
+    }
 
-	public Date getBeginDateTime() {
-		return beginDateTime;
-	}
+    public Date getBeginDateTime() {
+        return beginDateTime;
+    }
 
-	public void setBeginDateTime(Date beginDateTime) {
-		this.beginDateTime = beginDateTime;
-	}
+    public void setBeginDateTime(Date beginDateTime) {
+        this.beginDateTime = beginDateTime;
+    }
 
-	public Date getEndDateTime() {
-		return endDateTime;
-	}
+    public Date getEndDateTime() {
+        return endDateTime;
+    }
 
-	public void setEndDateTime(Date endDateTime) {
-		this.endDateTime = endDateTime;
-	}
+    public void setEndDateTime(Date endDateTime) {
+        this.endDateTime = endDateTime;
+    }
 
-	public Long getId() {
+    public Long getId() {
         return id;
     }
 
@@ -221,13 +221,19 @@ public class NewCustomCoupon {
         this.distributionModeId = distributionModeId;
     }
 
-	public Integer getCouponType() {
-		return couponType;
-	}
+    public Integer getCouponType() {
+        return couponType;
+    }
 
-	public void setCouponType(Integer couponType) {
-		this.couponType = couponType;
-	}
-    
-    
+    public void setCouponType(Integer couponType) {
+        this.couponType = couponType;
+    }
+
+    public Integer getIsBrand() {
+        return isBrand;
+    }
+
+    public void setIsBrand(Integer isBrand) {
+        this.isBrand = isBrand;
+    }
 }
