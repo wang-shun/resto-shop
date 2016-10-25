8c02be8ecd22895f01ab8fb24340778193b987a4
limingxin <906529775@qq.com>
2016-10-25 16:00:13 +0800

    .

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/CustomerServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/CustomerServiceImpl.java
@@ -39,6 +39,7 @@ public class CustomerServiceImpl extends GenericServiceImpl<Customer, String> im
 
 	@Override
 	public Customer login(String openid) {
+		log.info("\n\n----查询\n\n");
 		Customer customer = selectByOpenId(openid);
 		if(customer!=null){
 			Customer change = new Customer();
@@ -46,6 +47,7 @@ public class CustomerServiceImpl extends GenericServiceImpl<Customer, String> im
 			change.setLastLoginTime(new Date());
 			update(change);
 		}
+		log.info("\n\n----查询2\n\n");
 		return customer;
 	}
 	
@@ -74,6 +76,7 @@ public class CustomerServiceImpl extends GenericServiceImpl<Customer, String> im
 	
 	@Override
 	public Customer register(Customer customer) {
+		log.info("【register   ----   01】");
 		String customerId = ApplicationUtils.randomUUID();
 		customer.setId(customerId);
 		Account account = new Account();
@@ -85,6 +88,7 @@ public class CustomerServiceImpl extends GenericServiceImpl<Customer, String> im
 		customer.setLastLoginTime(new Date());
 		customer.setRegiestTime(new Date());
 		customer.setAccount(account.getRemain());
+		log.info("【register   ----   02】");
 		insert(customer);
 		return customer;
 	}
