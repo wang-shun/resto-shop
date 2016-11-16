cd19a8a61d3f45c8b1f5b7071865766bcebbca31
645534731 <645534731@qq.com>
2016-11-16 15:50:47 +0800

    王寅杰

--- resto-shop-server/src/main/java/com/resto/shop/web/consumer/OrderMessageListener.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/consumer/OrderMessageListener.java
@@ -219,9 +219,9 @@ public class OrderMessageListener implements MessageListener {
         msg.append("<a href='" + setting.getWechatWelcomeUrl() + "?shopId=" + customer.getLastOrderShop() + "&subpage=home&dialog=share&appraiseId=" + appraise.getId() + "'>再次领取红包</a>");
         log.info("异步发送分享好评微信通知ID:" + appraise.getId() + " 内容:" + msg);
         log.info("ddddd-"+customer.getWechatId()+"dddd-"+config.getAppid()+"dddd-"+config.getAppsecret());
-//        ShopDetail shopDetail = shopDetailService.selectById(appraise.getShopDetailId());
-//        logBaseService.insertLogBaseInfoState(shopDetail, customer, appraise.getId(), LogBaseState.SHARE);
+        ShopDetail shopDetail = shopDetailService.selectById(appraise.getShopDetailId());
         WeChatUtils.sendCustomerMsgASync(msg.toString(), customer.getWechatId(), config.getAppid(), config.getAppsecret());
+        logBaseService.insertLogBaseInfoState(shopDetail, customer, appraise.getId(), LogBaseState.SHARE);
         log.info("分享完毕:" );
     }
 
