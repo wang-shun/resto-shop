246b5ed224e59ebcd97dfab6ca411bf311231901
luyujie <125823888@qq.com>
2016-11-23 13:03:27 +0800

    update

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
@@ -713,7 +713,7 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
 
     private boolean validOrderCanPush(Order order) throws AppException {
 
-        if(order.getPayMode() == OrderPayMode.ALI_PAY
+        if(order.getPayMode() != null && order.getPayMode().equals(OrderPayMode.ALI_PAY)
                 && order.getProductionStatus().equals(ProductionStatus.NOT_ORDER) && order.getOrderState().equals(OrderState.SUBMIT)){ //支付宝支付
             return true;
         }
