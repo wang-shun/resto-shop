6626ca246924486b76056465d60db95afa59d2f3
645534731 <645534731@qq.com>
2016-12-01 18:28:32 +0800

    王寅杰

--- resto-shop-api/src/main/java/com/resto/shop/web/constant/ShopCarType.java
+++ resto-shop-api/src/main/java/com/resto/shop/web/constant/ShopCarType.java
@@ -8,4 +8,6 @@ public class ShopCarType {
     public static final String DANPIN = "1";  //单品
     public static final String TAOCAN = "2";  //套餐
     public static final String TAOCANDANPIN = "3";  //套餐单品
+    public static final String CAOBAO = "4";  //推荐餐包
+    public static final String CAOBAODANPIN = "5";  //推荐餐包单品
 }
