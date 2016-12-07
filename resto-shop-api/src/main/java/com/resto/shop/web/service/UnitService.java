e9850faf8f310fa324b254ce253c447dedc74b85
645534731 <645534731@qq.com>
2016-12-07 11:02:04 +0800

    王寅杰

--- resto-shop-api/src/main/java/com/resto/shop/web/service/UnitService.java
+++ resto-shop-api/src/main/java/com/resto/shop/web/service/UnitService.java
@@ -2,6 +2,7 @@ package com.resto.shop.web.service;
 
 import com.resto.brand.core.generic.GenericService;
 import com.resto.shop.web.model.Unit;
+import com.resto.shop.web.model.UnitArticle;
 import com.resto.shop.web.model.UnitDetail;
 
 import java.util.List;
@@ -31,5 +32,5 @@ public interface UnitService extends GenericService<Unit, String> {
 
    void modifyUnit(Unit unit);
 
-   UnitDetail selectUnitDetail(String id);
+   List<UnitArticle> selectUnitDetail(String id);
 }
