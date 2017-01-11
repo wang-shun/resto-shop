2b88ff7d8da5b292bf44cc3f95bb2b31e8421288
luyujie <125823888@qq.com>
2017-01-11 16:06:28 +0800

    合并代码

--- resto-shop-api/src/main/java/com/resto/shop/web/service/ChargePaymentService.java
+++ resto-shop-api/src/main/java/com/resto/shop/web/service/ChargePaymentService.java
@@ -12,7 +12,6 @@ public interface ChargePaymentService extends GenericService<ChargePayment, Stri
 	
 	RechargeLogDto selectRechargeLog(String beginDate,String endDate,String brandId);
     
-	RechargeLogDto selectRechargeLog(String beginDate,String endDate,String brandId);
-	
+
 	RechargeLogDto selectShopRechargeLog(String beginDate,String endDate,String shopId);
 }
