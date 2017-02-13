0fe34f6da7e79bc989129a10dd4b4e1dce699375
645534731 <645534731@qq.com>
2017-02-13 16:47:29 +0800

    bug-fixed

--- resto-shop-server/src/main/java/com/resto/shop/web/aspect/OrderAspect.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/aspect/OrderAspect.java
@@ -407,8 +407,13 @@ public class OrderAspect {
                 log.info("发送禁止加菜:" + setting.getCloseContinueTime() + "s 后发送");
                 if (order.getOrderMode() == ShopMode.BOSS_ORDER){
                     MQMessageProducer.sendNotAllowContinueMessage(order, 1000 * setting.getCloseContinueTime()); //延迟两小时，禁止继续加菜
-                    if(order.getOrderState() == OrderState.SUBMIT){
-                        MQMessageProducer.sendAutoConfirmOrder(order, setting.getAutoConfirmTime() * 1000*2);
+//                    if(order.getOrderState() == OrderState.SUBMIT){
+//                        MQMessageProducer.sendAutoConfirmOrder(order, setting.getAutoConfirmTime() * 1000*2);
+//                    }else{
+//                        MQMessageProducer.sendAutoConfirmOrder(order, setting.getAutoConfirmTime() * 1000);
+//                    }
+                    if(setting.getAutoConfirmTime() < setting.getCloseContinueTime()){
+                        MQMessageProducer.sendAutoConfirmOrder(order, setting.getCloseContinueTime() * 1000);
                     }else{
                         MQMessageProducer.sendAutoConfirmOrder(order, setting.getAutoConfirmTime() * 1000);
                     }
