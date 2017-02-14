e67a74a55585cb695e35a17b7bceeacc12f13809
645534731 <645534731@qq.com>
2017-02-14 17:53:26 +0800

    bug-fixed

--- resto-shop-server/src/main/java/com/resto/shop/web/aspect/OrderAspect.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/aspect/OrderAspect.java
@@ -412,8 +412,13 @@ public class OrderAspect {
 //                    }else{
 //                        MQMessageProducer.sendAutoConfirmOrder(order, setting.getAutoConfirmTime() * 1000);
 //                    }
-                    if(setting.getAutoConfirmTime() < setting.getCloseContinueTime()){
-                        MQMessageProducer.sendBossOrder(order, setting.getCloseContinueTime() * 1000 - 10000);
+                    if(setting.getAutoConfirmTime() < setting.getCloseContinueTime()){    //加菜时间跟领取红包时间对比
+                        if(order.getOrderState() == OrderState.SUBMIT){   //是否买单
+                            MQMessageProducer.sendNotAllowContinueMessage(order, 1000 * setting.getCloseContinueTime()); //延迟禁止继续加菜
+                            MQMessageProducer.sendAutoConfirmOrder(order, setting.getAutoConfirmTime() * 1000);
+                        }else if(order.getOrderState() == OrderState.PAYMENT){
+                            MQMessageProducer.sendBossOrder(order, setting.getCloseContinueTime() * 1000 - 10000);
+                        }
                     }else{
                         MQMessageProducer.sendNotAllowContinueMessage(order, 1000 * setting.getCloseContinueTime()); //延迟禁止继续加菜
                         MQMessageProducer.sendAutoConfirmOrder(order, setting.getAutoConfirmTime() * 1000);
