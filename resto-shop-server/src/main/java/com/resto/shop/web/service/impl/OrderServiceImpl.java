1e1bfe067e7f62ea13570c36329950cec8c35507
luyujie <125823888@qq.com>
2017-02-14 13:57:56 +0800

    修改菜品的日志记录

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
@@ -5633,7 +5633,7 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
             order.setOriginalAmount(order.getOriginalAmount().add(order.getServicePrice()));
 
             update(order);
-            UserActionUtils.write(logPath+"posAction",brand.getBrandName()
+            UserActionUtils.write("/logs/posAction",brand.getBrandName()
                     ,shopDetail.getName(),
                     shopDetail.getName()+"修改了"+setting.getServiceName()+"，数量修改为"+count+",订单号为:"+order.getId());
 
@@ -5682,7 +5682,7 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
 
             update(order);
 
-            UserActionUtils.write(logPath+"posAction",brand.getBrandName()
+            UserActionUtils.write("/logs/posAction",brand.getBrandName()
                     ,shopDetail.getName(),shopDetail.getName()+"修改了菜品"+orderItem.getArticleName()+"，数量修改为"+count+",订单号为:"+order.getId());
 
 
