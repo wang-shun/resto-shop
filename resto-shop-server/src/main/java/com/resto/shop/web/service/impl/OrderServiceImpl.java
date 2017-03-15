3314ed40085dbd5befabad91e40145a516734748
645534731 <645534731@qq.com>
2017-03-15 16:30:12 +0800

    bug-fixed

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
@@ -2074,7 +2074,7 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
 //            }
 //
 //        }
-        if(order.getPayType() == PayType.PAY && (order.getPayMode() == OrderPayMode.YL_PAY || order.getPayMode() == OrderPayMode.XJ_PAY)){
+        if(order.getPayType() == PayType.NOPAY && (order.getPayMode() == OrderPayMode.YL_PAY || order.getPayMode() == OrderPayMode.XJ_PAY)){
             data.put("RESTAURANT_NAME", shopDetail.getName() + " (结账单)");
         }else if(order.getOrderState() == OrderState.SUBMIT){
             data.put("RESTAURANT_NAME", shopDetail.getName() + " (消费清单)");
