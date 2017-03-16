eea7697f0b0c15b6ebedef74ec931cd3c63e1ff8
luyujie <125823888@qq.com>
2017-03-16 16:22:29 +0800

    虚拟餐品

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
@@ -1647,7 +1647,7 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
         String tableNumber = order.getTableNumber() != null ? order.getTableNumber() : "";
         List<Map<String, Object>> items = new ArrayList<Map<String, Object>>();
         List<OrderItem> orderItems = orderitemMapper.getOrderItemByRecommendId(recommendId, order.getId());
-
+        StringBuilder sb = new StringBuilder();
         for (OrderItem orderItem : orderItems) {
             String articleId = orderItem.getArticleId();
 
@@ -1658,6 +1658,7 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
             if (article.getVirtualId() == null) {
                 Map<String, Object> item = new HashMap<String, Object>();
                 item.put("ARTICLE_NAME", orderItem.getArticleName());
+                sb.append(orderItem.getArticleName()+" ");
                 item.put("ARTICLE_COUNT", orderItem.getCount());
                 items.add(item);
             }
@@ -1749,6 +1750,9 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
             printList = new ArrayList<>();
         }
         printList.add(print_id);
+        MemcachedUtils.put(print_id+"customer",customer);
+
+        MemcachedUtils.put(print_id+"article",sb.toString());
         MemcachedUtils.put(shopDetail.getId()+"printList",printList);
     }
 
@@ -1794,6 +1798,8 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
         print.put("TICKET_TYPE", TicketType.RESTAURANTLABEL);
         printTask.add(print);
         MemcachedUtils.put(print_id,print);
+        MemcachedUtils.put(print_id+"customer",customer);
+        MemcachedUtils.put(print_id+"article",article.getArticleName());
         List<String> printList = (List<String>)MemcachedUtils.get(shopDetail.getId()+"printList");
         if(printList == null){
             printList = new ArrayList<>();
@@ -1806,7 +1812,9 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
     private void getKitchenModel(OrderItem article, Order order, Printer printer, ShopDetail shopDetail, List<Map<String, Object>> printTask) {
         //保存 菜品的名称和数量
         List<Map<String, Object>> items = new ArrayList<Map<String, Object>>();
+        StringBuilder sb = new StringBuilder();
         Map<String, Object> item = new HashMap<String, Object>();
+        sb.append(article.getArticleName() + " ");
         item.put("ARTICLE_NAME", article.getArticleName());
         item.put("ARTICLE_COUNT", article.getCount());
         items.add(item);
@@ -1820,6 +1828,7 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
                 for (OrderItem obj : list) {
                     Map<String, Object> child_item = new HashMap<String, Object>();
                     child_item.put("ARTICLE_NAME", obj.getArticleName());
+                    sb.append(obj.getArticleName() + " ");
                     if (order.getIsRefund() != null && order.getIsRefund() == Common.YES) {
                         child_item.put("ARTICLE_COUNT", obj.getRefundCount());
                     } else {
@@ -1834,6 +1843,7 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
                 for (OrderItem obj : article.getChildren()) {
                     Map<String, Object> child_item = new HashMap<String, Object>();
                     child_item.put("ARTICLE_NAME", obj.getArticleName());
+                    sb.append(obj.getArticleName() + " ");
                     if (order.getIsRefund() != null && order.getIsRefund() == Common.YES) {
                         child_item.put("ARTICLE_COUNT", obj.getRefundCount());
                     } else {
@@ -1924,6 +1934,8 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
         printTask.add(print);
 
         MemcachedUtils.put(print_id,print);
+        MemcachedUtils.put(print_id+"customer",customer);
+        MemcachedUtils.put(print_id+"article",sb.toString());
         List<String> printList = (List<String>)MemcachedUtils.get(shopDetail.getId()+"printList");
         if(printList == null){
             printList = new ArrayList<>();
