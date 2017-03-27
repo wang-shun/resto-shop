e85746657aeabda5b1e13210f22446cf892a6bef
limingxin <906529775@qq.com>
2017-03-27 14:52:37 +0800

    .

--- resto-shop-api/src/main/java/com/resto/shop/web/service/PlatformOrderExtraService.java
+++ resto-shop-api/src/main/java/com/resto/shop/web/service/PlatformOrderExtraService.java
@@ -1,6 +1,7 @@
 package com.resto.shop.web.service;
 
 import com.resto.brand.core.generic.GenericService;
+import com.resto.brand.web.dto.MeiTuanOrderDto;
 import com.resto.shop.web.model.PlatformOrderExtra;
 
 import java.util.List;
@@ -14,5 +15,5 @@ public interface PlatformOrderExtraService extends GenericService<PlatformOrderE
      */
     List<PlatformOrderExtra> selectByPlatformOrderId(String platformOrderId);
 
-    void meituanOrderExtra(String orderId,String orderExtra);
+    void meituanOrderExtra(MeiTuanOrderDto orderDto);
 }
