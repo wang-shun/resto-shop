2f1ca5dc351fed987d88b5fceeefe2e8a2e3345a
luyujie <125823888@qq.com>
2017-03-28 15:46:57 +0800

    bugfixed

--- resto-shop-server/src/main/java/com/resto/shop/web/aspect/OrderAspect.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/aspect/OrderAspect.java
@@ -336,7 +336,8 @@ public class OrderAspect {
             MQMessageProducer.sendPlaceOrderMessage(order);
         }
         if(order.getPayType() == PayType.NOPAY && order.getOrderMode() == ShopMode.BOSS_ORDER && (order.getPayMode() == OrderPayMode.WX_PAY || order.getPayMode() == OrderPayMode.ALI_PAY)){
-            orderService.confirmOrder(order);
+            MQMessageProducer.sendAutoConfirmOrder(order, 20 * 1000);
+//            orderService.confirmOrder(order);
         }
 
 
