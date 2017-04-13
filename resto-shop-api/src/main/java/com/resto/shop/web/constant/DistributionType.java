af027a0bf7093a338c66024fb3c3a380fc45cb88
13317182430@163.com <2440@163.com>
2017-04-13 12:02:02 +0800

    新增mybaits的分页插件 测试--

--- resto-shop-api/src/main/java/com/resto/shop/web/constant/DistributionType.java
+++ resto-shop-api/src/main/java/com/resto/shop/web/constant/DistributionType.java
@@ -4,7 +4,7 @@ public class DistributionType {
 	public final static int RESTAURANT_MODE_ID = 1;  //堂吃
 	public final static int DELIVERY_MODE_ID = 2;    //自提外卖
 	public static final int TAKE_IT_SELF = 3;		//外带
-	public static final int REFUND_ORDER = 4;		//外带
+	public static final int REFUND_ORDER = 4;		//退菜
 	
 	
 	public static String getModeText(int mid){
