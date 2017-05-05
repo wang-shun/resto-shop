818d375583a1b75b701edfd72291341766527eed
645534731 <645534731@qq.com>
2017-05-05 16:34:31 +0800

    bug-fixed

--- resto-shop-server/src/main/java/com/resto/shop/web/aspect/OrderAspect.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/aspect/OrderAspect.java
@@ -321,8 +321,8 @@ public class OrderAspect {
 
     @AfterReturning(value = "posPayOrder()", returning = "order")
     public void posPayOrder(Order order) {
-        WeChatUtils.sendCustomerMsgASync("测试测试！", "oBHT9sqENykH4eDytavxr7nlmeKs", "wx36bd5b9b7d264a8c", "807530431fe6e19e3f2c4a7d1a149465");
-        MQMessageProducer.sendPlaceOrderMessage(order);
+//        WeChatUtils.sendCustomerMsgASync("测试测试！", "oBHT9sqENykH4eDytavxr7nlmeKs", "wx36bd5b9b7d264a8c", "807530431fe6e19e3f2c4a7d1a149465");
+        MQMessageProducer.sendPlaceOrderNoPayMessage(order);
     }
 
     @AfterReturning(value = "orderWxPaySuccess()", returning = "order")
