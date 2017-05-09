ae8d94043e89635a009210b1e25a1ac5df97e56d
WuTingLong <15123849097>
2017-05-09 10:49:01 +0800

    update

--- resto-shop-api/src/main/java/com/resto/shop/web/service/OrderRemarkService.java
+++ resto-shop-api/src/main/java/com/resto/shop/web/service/OrderRemarkService.java
@@ -1,5 +1,6 @@
 package com.resto.shop.web.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.resto.brand.core.generic.GenericService;
 import com.resto.shop.web.model.OrderRemark;
 
@@ -8,6 +9,8 @@ import java.util.List;
 public interface OrderRemarkService extends GenericService<OrderRemark, String>{
 
     List<OrderRemark> selectOrderRemarkByShopId(String shopId);
+
+    List<JSONObject> getShopOrderRemark(String shopId);
 
     void deleteByBoOrderRemarkId(String boOrderRemarkId);
 }
--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderRemarkServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderRemarkServiceImpl.java
@@ -1,6 +1,8 @@
 package com.resto.shop.web.service.impl;
 
 import cn.restoplus.rpc.server.RpcService;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.resto.brand.core.generic.GenericDao;
 import com.resto.brand.core.generic.GenericServiceImpl;
 import com.resto.shop.web.dao.OrderRemarkMapper;
@@ -8,6 +10,7 @@ import com.resto.shop.web.model.OrderRemark;
 import com.resto.shop.web.service.OrderRemarkService;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @RpcService
@@ -15,6 +18,9 @@ public class OrderRemarkServiceImpl extends GenericServiceImpl<OrderRemark, Stri
 
 	@Resource
 	private OrderRemarkMapper orderRemarkMapper;
+
+    @Resource
+    private com.resto.brand.web.service.OrderRemarkService boOrderRemarkService;
 	
 	@Override
 	public GenericDao<OrderRemark, String> getDao() {
@@ -24,6 +30,26 @@ public class OrderRemarkServiceImpl extends GenericServiceImpl<OrderRemark, Stri
     @Override
     public List<OrderRemark> selectOrderRemarkByShopId(String shopId) {
         return orderRemarkMapper.selectOrderRemarkByShopId(shopId);
+    }
+
+    @Override
+    public List<JSONObject> getShopOrderRemark(String shopId) {
+        List<JSONObject> objectList = new ArrayList<>();
+        List<OrderRemark> shopOrderRemarks = orderRemarkMapper.selectOrderRemarkByShopId(shopId);
+        List<com.resto.brand.web.model.OrderRemark> brandOrderRemarks = boOrderRemarkService.selectOrderRemarks();
+        for (com.resto.brand.web.model.OrderRemark boOrderRemark : brandOrderRemarks){
+            if (!shopOrderRemarks.isEmpty()){
+                for (OrderRemark orderRemark : shopOrderRemarks){
+                    if (orderRemark.getBoOrderRemarkId().equalsIgnoreCase(boOrderRemark.getId())){
+                        objectList.add(JSON.parseObject(JSON.toJSONString(boOrderRemark)));
+                        break;
+                    }
+                }
+            }else{
+                break;
+            }
+        }
+        return objectList;
     }
 
     @Override
