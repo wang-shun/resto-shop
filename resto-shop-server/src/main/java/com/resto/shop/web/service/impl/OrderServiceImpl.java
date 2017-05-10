d8698753762d8e4e48aa9eed5a0335b7292e2804
645534731 <645534731@qq.com>
2017-05-10 17:48:18 +0800

    bug-fixed

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
@@ -364,7 +364,7 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
         }
 
         if (brandSetting.getIsUseServicePrice() == Common.YES && shopDetail.getIsUseServicePrice() == Common.YES
-                && (order.getCustomerCount() == 0 || order.getCustomerCount() == null)
+                && (order.getCustomerCount() == null || order.getCustomerCount() == 0)
                 && order.getDistributionModeId() == DistributionType.RESTAURANT_MODE_ID) {
             jsonResult.setSuccess(false);
             jsonResult.setMessage("请输入就餐人数！");
