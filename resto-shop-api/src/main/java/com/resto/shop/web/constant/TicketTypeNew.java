1039e9c689cded43f832d28d2264c2a6230db06c
luyujie <125823888@qq.com>
2017-05-15 17:48:49 +0800

    tt

--- resto-shop-api/src/main/java/com/resto/shop/web/constant/TicketTypeNew.java
+++ resto-shop-api/src/main/java/com/resto/shop/web/constant/TicketTypeNew.java
@@ -8,6 +8,8 @@ public class TicketTypeNew {
 
     public static final String LABEL = "Label";
 
+    public static final String COMMAND = "Command";
+
 
     public static final String RESTAURANT_RECEIPT = "RestaurantReceipt";
 
@@ -17,5 +19,5 @@ public class TicketTypeNew {
 
     public static final String RESTAURANT_LABEL = "RestaurantLabel";
 
-    public static final String OPEN_CASH_DRAWER= "openCashDrawer";
+    public static final String OPEN_CASH_DRAWER= "OpenCashDrawer";
 }
