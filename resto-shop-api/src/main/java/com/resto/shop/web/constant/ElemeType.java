2583a2b29dbd95cb93a411506c6c0a760feec74d
645534731 <645534731@qq.com>
2017-05-16 13:57:39 +0800

    bug-fixed

---
+++ resto-shop-api/src/main/java/com/resto/shop/web/constant/ElemeType.java
@@ -0,0 +1,10 @@
+package com.resto.shop.web.constant;
+
+/**
+ * Created by carl on 2017/5/16.
+ */
+public class ElemeType {
+
+    public static final Integer NEW_ORDER = 10;    //订单生效，店铺可以看到新订单
+
+}
