5bf9a2043b50383b1550597533fdd430d5f022ed
645534731 <645534731@qq.com>
2017-05-19 19:16:57 +0800

    bug-fixed

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/ThirdServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/ThirdServiceImpl.java
@@ -739,19 +739,19 @@ public class ThirdServiceImpl implements ThirdService {
             type = Integer.parseInt(map.get("type").toString());
         }
         if(type == ElemeType.NEW_ORDER){
-            OMessage oMessage = (OMessage) map.get("oMessage");
-            String message = oMessage.getMessage();
-            message = message.replaceAll("\\\\","");
-            com.alibaba.fastjson.JSONObject messageJson = com.alibaba.fastjson.JSONObject.parseObject(message);
-            String orderId = messageJson.getString("orderId");
+            String oMessage =  map.get("oMessage").toString();
+            com.alibaba.fastjson.JSONObject messageJson = com.alibaba.fastjson.JSONObject.parseObject(oMessage);
+            String message = messageJson.getString("message");
+            com.alibaba.fastjson.JSONObject mj = com.alibaba.fastjson.JSONObject.parseObject(message);
+            String orderId = mj.getString("orderId");
             addHungerOrderVersion2(orderId);
         }else if(type == ElemeType.RECEIVE_ORDER){
             String shopId = shopDetailService.selectByOOrderShopId(Long.parseLong(map.get("shopId").toString())).getId();
-            OMessage oMessage = (OMessage) map.get("oMessage");
-            String message = oMessage.getMessage();
-            message = message.replaceAll("\\\\","");
-            com.alibaba.fastjson.JSONObject messageJson = com.alibaba.fastjson.JSONObject.parseObject(message);
-            String orderId = messageJson.getString("orderId");
+            String oMessage =  map.get("oMessage").toString();
+            com.alibaba.fastjson.JSONObject messageJson = com.alibaba.fastjson.JSONObject.parseObject(oMessage);
+            String message = messageJson.getString("message");
+            com.alibaba.fastjson.JSONObject mj = com.alibaba.fastjson.JSONObject.parseObject(message);
+            String orderId = mj.getString("orderId");
             MQMessageProducer.sendPlatformOrderMessage(orderId, PlatformType.E_LE_ME, brandId, shopId);
         }
         return true;
