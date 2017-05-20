0636c31f3883f53e65a3d7de353476b0449f3503
645534731 <645534731@qq.com>
2017-05-20 10:52:21 +0800

    bug-fixed

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/ThirdServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/ThirdServiceImpl.java
@@ -748,7 +748,9 @@ public class ThirdServiceImpl implements ThirdService {
         if(type == ElemeType.NEW_ORDER){
             addHungerOrderVersion2(orderId);
         }else if(type == ElemeType.RECEIVE_ORDER){
+            System.out.println("eleme订单orderId：" + orderId);
             String shopId = shopDetailService.selectByOOrderShopId(Long.parseLong(map.get("shopId").toString())).getId();
+            System.out.println("eleme订单下单对面餐加系统店铺：" + shopId);
             MQMessageProducer.sendPlatformOrderMessage(orderId, PlatformType.E_LE_ME, brandId, shopId);
         }
         return true;
