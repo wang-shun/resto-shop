2a8b394581efcdba702fd94ea1af6cb17044e34e
645534731 <645534731@qq.com>
2017-06-20 11:14:13 +0800

    bug-fixed

--- resto-shop-api/src/main/java/com/resto/shop/web/constant/DistributionType.java
+++ resto-shop-api/src/main/java/com/resto/shop/web/constant/DistributionType.java
@@ -2,7 +2,7 @@ package com.resto.shop.web.constant;
 
 public class DistributionType {
 	public final static int RESTAURANT_MODE_ID = 1;  //堂吃
-	public final static int DELIVERY_MODE_ID = 2;    //自提外卖
+	public final static int DELIVERY_MODE_ID = 2;    //外卖
 	public static final int TAKE_IT_SELF = 3;		//外带
 	public static final int REFUND_ORDER = 4;		//退菜
     public static final int MODIFY_ORDER = 5;		//pos端增加订单菜品数量
@@ -14,7 +14,7 @@ public class DistributionType {
 		case RESTAURANT_MODE_ID:
 			return "堂吃";
 		case DELIVERY_MODE_ID:
-			return "自提外卖";
+			return "外卖";
 		case TAKE_IT_SELF:
 			return "外带";
 		case REFUND_ORDER:
