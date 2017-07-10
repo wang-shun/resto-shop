a64341baba1afc484378f1f67ca1980142df4364
645534731 <645534731@qq.com>
2017-07-10 15:58:58 +0800

    bug-fixed

--- resto-shop-api/src/main/java/com/resto/shop/web/service/WaitPictureService.java
+++
@@ -1,8 +0,0 @@
-package com.resto.shop.web.service;
-
-import com.resto.brand.core.generic.GenericService;
-import com.resto.shop.web.model.WaitPicture;
-
-public interface WaitPictureService extends GenericService<WaitPicture, Integer> {
-    
-}
