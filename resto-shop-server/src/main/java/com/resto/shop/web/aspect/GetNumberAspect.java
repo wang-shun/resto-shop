20b4e5f02860610b5cf1746c0e6deeb3c919d3fe
645534731 <645534731@qq.com>
2017-07-11 15:50:26 +0800

    bug-fixed

--- resto-shop-server/src/main/java/com/resto/shop/web/aspect/GetNumberAspect.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/aspect/GetNumberAspect.java
@@ -108,7 +108,7 @@ public class GetNumberAspect {
 
             if(shop.getWaitRemindSwitch() == 1 && shop.getWaitRemindNumber() > 0 &&
                     (getNumber.getState() == WaitModerState.WAIT_MODEL_NUMBER_ONE || getNumber.getState() == WaitModerState.WAIT_MODEL_NUMBER_TWO)){
-                List<GetNumber> getNumberList = getNumberService.selectBeforeNumberByCodeId(getNumber.getShopDetailId(), getNumber.getCodeId(), getNumber.getCreateTime());
+                List<GetNumber> getNumberList = getNumberService.selectAfterNumberByCodeId(getNumber.getShopDetailId(), getNumber.getCodeId(), getNumber.getCreateTime());
                 if((getNumberList.size() + 1) >= shop.getWaitRemindNumber()){
                     GetNumber gn = getNumberList.get(shop.getWaitRemindNumber() - 1);
                     Customer c = customerService.selectById(gn.getCustomerId());
