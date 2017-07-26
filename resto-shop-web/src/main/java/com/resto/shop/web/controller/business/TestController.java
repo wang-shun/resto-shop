a0e1571880e059d81669fe764d591460b4862b51
13317182430@163.com <2440@163.com>
2017-07-26 11:19:42 +0800

    日结数据优化

---
+++ resto-shop-web/src/main/java/com/resto/shop/web/controller/business/TestController.java
@@ -0,0 +1,18 @@
+ package com.resto.shop.web.controller.business;
+
+ import com.resto.shop.web.controller.GenericController;
+ import org.springframework.stereotype.Controller;
+ import org.springframework.web.bind.annotation.RequestMapping;
+
+
+ @Controller
+ @RequestMapping("test")
+ /**
+  * 
+  */
+ public class TestController extends GenericController{
+
+
+
+
+ }
