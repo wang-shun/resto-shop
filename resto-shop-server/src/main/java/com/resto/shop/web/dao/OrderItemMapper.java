5f03d92debccd4f7871b09b7734bca5293c030d3
WuTingLong <15123849097>
2017-07-27 11:36:38 +0800

    update

--- resto-shop-server/src/main/java/com/resto/shop/web/dao/OrderItemMapper.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/dao/OrderItemMapper.java
@@ -79,4 +79,6 @@ public interface OrderItemMapper  extends GenericDao<OrderItem,String> {
     List<OrderItem> getListByRecommendId(@Param("recommendId") String recommendId,@Param("orderId") String orderId);
 
     List<OrderItem> selectRefundArticleItem(@Param("orderId") String orderId);
+
+    List<OrderItem> selectByArticleIds(String[] articleIds);
 }
