40cb5116dd1a4ee8e50fdc34a5e01bf0d5b7c4e0
xieluncheng <xlc_sscf8888@163.com>
2017-08-04 15:16:41 +0800

    优惠券cd

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/ArticleServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/ArticleServiceImpl.java
@@ -168,11 +168,16 @@ public class ArticleServiceImpl extends GenericServiceImpl<Article, String> impl
     @Override
     public List<Article> selectListByShopIdRecommendCategory(String currentShopId, String recommendCcategoryId, String show) {
         List<Article> articleList = articleMapper.selectListByShopIdRecommendCategory(currentShopId, recommendCcategoryId);
+        //当前时间的年月
+        Date d = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
+        String dateNowStr = sdf.format(d);
         for (Article article : articleList) {
             Integer count = (Integer) RedisUtil.get(article.getId() + Common.KUCUN);
             if (count != null) {
                 article.setCurrentWorkingStock(count);
             }
+            article.setMonthlySales(articleMapper.selectSumByMonthlySales(article.getId(), dateNowStr));
             article.setRecommendCategoryId(recommendCcategoryId);
         }
         getArticleDiscount(currentShopId, articleList, show);
