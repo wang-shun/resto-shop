014e1f706ea55c5b916346114a953959d446ee8b
645534731 <645534731@qq.com>
2017-08-24 13:47:53 +0800

    121212

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/PrinterServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/PrinterServiceImpl.java
@@ -56,18 +56,20 @@ public class PrinterServiceImpl extends GenericServiceImpl<Printer, Integer> imp
 	public Map<String, Object> openCashDrawer(String orderId,String shopId) {
     	Printer printer = printerMapper.getCashPrinter(shopId);
 		Map<String, Object> result = new HashMap();
-		result.put("TABLE_NO","2");
-		result.put("KITCHEN_NAME",printer.getName());
-		result.put("PORT",printer.getPort());
-		result.put("ORDER_ID",orderId);
-		result.put("IP",printer.getIp());
-		String print_id = ApplicationUtils.randomUUID();
-		result.put("PRINT_TASK_ID", print_id);
-		result.put("ADD_TIME", new Date().getTime());
-		Map<String, Object> data = new HashMap<>();
-		result.put("DATA",data);
-		result.put("STATUS", 0);
-		result.put("TICKET_TYPE", TicketType.OPENCASHDRAW);
+		if(printer != null){
+			result.put("TABLE_NO","2");
+			result.put("KITCHEN_NAME",printer.getName());
+			result.put("PORT",printer.getPort());
+			result.put("ORDER_ID",orderId);
+			result.put("IP",printer.getIp());
+			String print_id = ApplicationUtils.randomUUID();
+			result.put("PRINT_TASK_ID", print_id);
+			result.put("ADD_TIME", new Date().getTime());
+			Map<String, Object> data = new HashMap<>();
+			result.put("DATA",data);
+			result.put("STATUS", 0);
+			result.put("TICKET_TYPE", TicketType.OPENCASHDRAW);
+		}
 		return result;
 	}
 
