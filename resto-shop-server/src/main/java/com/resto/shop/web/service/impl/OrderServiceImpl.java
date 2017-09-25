c43134a3f5d9a17056df3586237d09a35a2a28e3
luyujie <125823888@qq.com>
2017-09-25 18:26:30 +0800

    ttt

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
@@ -574,17 +574,12 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
                     //判断折扣是否匹配，如果不匹配则不允许买单
                     a = articleMap.get(item.getArticleId());
                     item.setArticleName(item.getName());
-//                    if(a.getDiscount() != 100){
-//                        org_price = a.getPrice();
-//                    }else if (a.getFansPrice() != null) {
-//                        org_price = item.getPrice().subtract(a.getFansPrice()).add(a.getPrice());
-//                    } else {
-//                        org_price = item.getPrice();
-//                    }
-                    if (item.getDiscount() < 100) {
-                        org_price = item.getPrice().divide(new BigDecimal(item.getDiscount())).multiply(new BigDecimal(100));
-                    } else {
+                    if(a.getDiscount() != 100){
+                        org_price = a.getPrice();
+                    }else if (a.getFansPrice() != null) {
                         org_price = item.getPrice().subtract(a.getFansPrice()).add(a.getPrice());
+                    } else {
+                        org_price = item.getPrice();
                     }
                     price = item.getPrice();
                     fans_price = item.getPrice();
