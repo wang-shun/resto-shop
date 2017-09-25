e2e2e75466cd1f794a2ca1424ac402d5b810f2cc
645534731 <645534731@qq.com>
2017-09-25 13:09:50 +0800

    121212

---
+++ resto-shop-api/src/main/java/com/resto/shop/web/model/Participant.java
@@ -0,0 +1,75 @@
+package com.resto.shop.web.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by carl on 2017/9/25.
+ */
+public class Participant implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String groupId;
+
+    private String customerId;
+
+    private String orderId;
+
+    private String shopDetailId;
+
+    private String brandId;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getShopDetailId() {
+        return shopDetailId;
+    }
+
+    public void setShopDetailId(String shopDetailId) {
+        this.shopDetailId = shopDetailId;
+    }
+
+    public String getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(String brandId) {
+        this.brandId = brandId;
+    }
+}
---
+++ resto-shop-api/src/main/java/com/resto/shop/web/service/ParticipantService.java
@@ -0,0 +1,10 @@
+package com.resto.shop.web.service;
+
+import com.resto.brand.core.generic.GenericService;
+import com.resto.shop.web.model.Participant;
+
+/**
+ * Created by carl on 2017/9/25.
+ */
+public interface ParticipantService extends GenericService<Participant, Long> {
+}
