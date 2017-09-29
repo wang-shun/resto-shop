a72e3d0ae10ba057d55d13129423727b9d6c3035
645534731 <645534731@qq.com>
2017-09-29 11:53:11 +0800

    121212

--- resto-shop-api/src/main/java/com/resto/shop/web/constant/AccountLogType.java
+++ resto-shop-api/src/main/java/com/resto/shop/web/constant/AccountLogType.java
@@ -3,4 +3,5 @@ package com.resto.shop.web.constant;
 public class AccountLogType {
 	public final static int PAY=2;  //支出
 	public final static int INCOME=1; //收入
+	public final static int FROZEN=3; //冻结
 }
