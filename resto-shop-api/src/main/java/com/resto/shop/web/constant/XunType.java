bc578e3facabf114a919253d23cb6d6f683ad6a7
13317182430@163.com <2440@163.com>
2017-10-19 14:35:28 -0700

    代码 规范

---
+++ resto-shop-api/src/main/java/com/resto/shop/web/constant/XunType.java
@@ -0,0 +1,16 @@
+package com.resto.shop.web.constant;
+
+/**
+ * Created by yanjuan 2017-10-18
+ *
+ */
+public class XunType {
+
+    public static final Integer EARLY = 1;    //上旬
+
+    public static final Integer MIDDLE = 2;    //中旬
+
+    public static final Integer LAST = 3;//下旬
+
+
+}
