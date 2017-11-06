4e7bc8c06fc92be5e61668aa6cc9b05ae9bc165a
[tring] <[2473085065@qq.com]>
2017-11-06 10:30:36 +0800

    临时目录

---
+++ resto-shop-web/src/main/java/com/resto/shop/web/controller/scm/ScmDocPmsPoHeaderController.java
@@ -0,0 +1,61 @@
+ package com.resto.shop.web.controller.scm;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.resto.shop.web.controller.GenericController;
+import com.resto.brand.core.entity.Result;
+import com.resto.scm.web.model.DocPmsPoHeader;
+import com.resto.scm.web.service.DocPmsPoHeaderService;
+
+@Controller
+@RequestMapping("docPmsPoHeader")
+public class DocPmsPoHeaderController extends GenericController{
+
+	@Resource
+	DocPmsPoHeaderService docPmsPoHeaderService;
+	
+	@RequestMapping("/list")
+    public void list(){
+    }
+
+	@RequestMapping("/list_all")
+	@ResponseBody
+	public List<DocPmsPoHeader> listData(){
+		return docPmsPoHeaderService.selectList();
+	}
+	
+	@RequestMapping("list_one")
+	@ResponseBody
+	public Result list_one(Integer id){
+		DocPmsPoHeader docPmsPoHeader = docPmsPoHeaderService.selectById(id);
+		return getSuccessResult(docPmsPoHeader);
+	}
+	
+	@RequestMapping("create")
+	@ResponseBody
+	public Result create(@Valid DocPmsPoHeader docPmsPoHeader){
+		docPmsPoHeaderService.insert(docPmsPoHeader);
+		return Result.getSuccess();
+	}
+	
+	@RequestMapping("modify")
+	@ResponseBody
+	public Result modify(@Valid DocPmsPoHeader docPmsPoHeader){
+		docPmsPoHeaderService.update(docPmsPoHeader);
+		return Result.getSuccess();
+	}
+	
+	@RequestMapping("delete")
+	@ResponseBody
+	public Result delete(Integer id){
+		docPmsPoHeaderService.delete(id);
+		return Result.getSuccess();
+	}
+}
