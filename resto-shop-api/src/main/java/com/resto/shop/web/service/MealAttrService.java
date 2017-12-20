9e6a314ad2e04335fcb22e7f63bc3d22864a6f59
645534731 <645534731@qq.com>
2017-12-20 14:37:59 +0800

    121

--- resto-shop-api/src/main/java/com/resto/shop/web/service/MealAttrService.java
+++ resto-shop-api/src/main/java/com/resto/shop/web/service/MealAttrService.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import com.resto.brand.core.generic.GenericService;
 import com.resto.brand.web.dto.ArticleSellDto;
+import com.resto.shop.web.model.Article;
 import com.resto.shop.web.model.MealAttr;
 
 public interface MealAttrService extends GenericService<MealAttr, Integer> {
@@ -16,7 +17,7 @@ public interface MealAttrService extends GenericService<MealAttr, Integer> {
 	void deleteByIds(List<Integer> ids);
 
 
-	List<MealAttr> selectFullByArticleId(String articleId,String show);
+	List<MealAttr> selectFullByArticleId(String articleId,String show,Map<String, Article> articleMap);
 
 
 	List<ArticleSellDto> queryArticleMealAttr(Map<String, Object> selectMap);
