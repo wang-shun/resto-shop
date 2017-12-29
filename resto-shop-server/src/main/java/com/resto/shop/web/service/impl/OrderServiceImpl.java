28c0a6b0dd92014ab4681a9e69c55e7a87bda45c
645534731 <645534731@qq.com>
2017-12-29 13:44:51 +0800

    121

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
@@ -3604,17 +3604,17 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
         }
         switch (order.getDistributionModeId()) {
             case DistributionType.RESTAURANT_MODE_ID:
-                if(printer.getSupportTangshi().equals(Common.NO)){
+                if(printer.getSupportTangshi() == 0){
                     return null;
                 }
                 break;
             case DistributionType.TAKE_IT_SELF:
-                if(printer.getSupportWaidai().equals(Common.NO)){
+                if(printer.getSupportWaidai() == 0){
                     return null;
                 }
                 break;
             case DistributionType.DELIVERY_MODE_ID:
-                if(printer.getSupportWaimai().equals(Common.NO)){
+                if(printer.getSupportWaimai() == 0){
                     return null;
                 }
                 break;
@@ -4015,17 +4015,17 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
     public Map<String, Object> printTicketPosNew(Order order, List<OrderItem> orderItems, ShopDetail shopDetail, Printer printer) {
         switch (order.getDistributionModeId()) {
             case DistributionType.RESTAURANT_MODE_ID:
-                if(printer.getSupportTangshi().equals(Common.NO)){
+                if(printer.getSupportTangshi() == 0){
                     return null;
                 }
                 break;
             case DistributionType.TAKE_IT_SELF:
-                if(printer.getSupportWaidai().equals(Common.NO)){
+                if(printer.getSupportWaidai() == 0){
                     return null;
                 }
                 break;
             case DistributionType.DELIVERY_MODE_ID:
-                if(printer.getSupportWaimai().equals(Common.NO)){
+                if(printer.getSupportWaimai() == 0){
                     return null;
                 }
                 break;
