ea625e224af0dacedfae51bac0b8e33325079574
645534731 <645534731@qq.com>
2018-01-03 17:25:39 +0800

    121

--- resto-shop-server/src/main/java/com/resto/shop/web/aspect/OrderAspect.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/aspect/OrderAspect.java
@@ -994,10 +994,9 @@ public class OrderAspect {
 //                        MQMessageProducer.sendAutoConfirmOrder(order, setting.getAutoConfirmTime() * 1000);
 //                    }
                     if (setting.getAutoConfirmTime() <= setting.getCloseContinueTime()) {    //加菜时间跟领取红包时间对比
-                        if (order.getOrderState() == OrderState.SUBMIT) {   //是否买单
+                        if (order.getOrderState() == OrderState.PAYMENT) {
                             MQMessageProducer.sendNotAllowContinueMessage(order, 1000 * setting.getCloseContinueTime()); //延迟禁止继续加菜
-                        } else if (order.getOrderState() == OrderState.PAYMENT) {
-                            MQMessageProducer.sendBossOrder(order, setting.getCloseContinueTime() * 1000 - 10000);
+                            MQMessageProducer.sendBossOrder(order, setting.getCloseContinueTime() * 1000);
                         }
                     } else {
                         MQMessageProducer.sendNotAllowContinueMessage(order, 1000 * setting.getCloseContinueTime()); //延迟禁止继续加菜
--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
@@ -4511,7 +4511,6 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
             return null;
         }
         Brand brand = brandService.selectById(order.getBrandId());
-        ShopDetail shopDetail = shopDetailService.selectById(order.getShopDetailId());
         log.info("开始确认订单:" + order.getId());
         Integer orginState = order.getOrderState();//订单开始确认的状体
         if (order.getConfirmTime() == null && !order.getClosed()) {
@@ -4520,7 +4519,6 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
             order.setAllowCancel(false);
             BrandSetting setting = brandSettingService.selectByBrandId(order.getBrandId());
             if (order.getParentOrderId() == null) {
-                log.info("如果订单金额大于 评论金额 则允许评论" + order.getId());
                 if (setting.getAppraiseMinMoney().compareTo(order.getOrderMoney()) <= 0 || setting.getAppraiseMinMoney().compareTo(order.getAmountWithChildren()) <= 0) {
                     order.setAllowAppraise(true);
                 }
@@ -4529,12 +4527,6 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
                 order.setAllowAppraise(false);
             }
             update(order);
-            //Map orderMap = new HashMap(4);
-//            orderMap.put("brandName", brand.getBrandName());
-//            orderMap.put("fileName", order.getId());
-//            orderMap.put("type", "orderAction");
-//            orderMap.put("content", "订单:" + order.getId() + "被确认订单状态更改为10,请求服务器地址为:" + MQSetting.getLocalIP());
-//            doPostAnsc(url, orderMap);
             /**
              * 记录订单自动确认2-10过程
              */
@@ -4573,12 +4565,6 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
                 order.setAllowAppraise(false);
             }
             update(order);
-            //Map orderMap = new HashMap(4);
-//            orderMap.put("brandName", brand.getBrandName());
-//            orderMap.put("fileName", order.getId());
-//            orderMap.put("type", "orderAction");
-//            orderMap.put("content", "订单:" + order.getId() + "被确认订单状态更改为10,请求服务器地址为:" + MQSetting.getLocalIP());
-//            doPostAnsc(url, orderMap);
             /**
              * 记录订单自动确认2-10过程
              */
@@ -4603,16 +4589,13 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
         }
         Integer orginState = order.getOrderState();
         Brand brand = brandService.selectById(order.getBrandId());
-        ShopDetail shopDetail = shopDetailService.selectById(order.getShopDetailId());
         log.info("开始确认订单:" + order.getId());
         if (order.getConfirmTime() == null && !order.getClosed()) {
             order.setOrderState(OrderState.CONFIRM);
             order.setConfirmTime(new Date());
             order.setAllowCancel(false);
-            order.setAllowContinueOrder(false);
             BrandSetting setting = brandSettingService.selectByBrandId(order.getBrandId());
             if (order.getParentOrderId() == null) {
-                log.info("如果订单金额大于 评论金额 则允许评论" + order.getId());
                 if (setting.getAppraiseMinMoney().compareTo(order.getOrderMoney()) <= 0 || setting.getAppraiseMinMoney().compareTo(order.getAmountWithChildren()) <= 0) {
                     order.setAllowAppraise(true);
                 }
@@ -4621,12 +4604,6 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
                 order.setAllowAppraise(false);
             }
             update(order);
-//            Map orderMap = new HashMap(4);
-//            orderMap.put("brandName", brand.getBrandName());
-//            orderMap.put("fileName", order.getId());
-//            orderMap.put("type", "orderAction");
-//            orderMap.put("content", "订单:" + order.getId() + "被确认订单状态更改为10,请求服务器地址为:" + MQSetting.getLocalIP());
-//            doPostAnsc(url, orderMap);
             LogTemplateUtils.getConfirmByOrderType(brand.getBrandName(), order, orginState, "confirmBossOrder");
             if(!StringUtils.isEmpty(order.getGroupId())){
                 //如果订单是在组里的
