ffedd11e08dbd0b9035b823ee31af8cad442ede5
xiaonan <645534731@qq.com>
2018-01-15 17:19:34 +0800

    121

--- resto-shop-server/src/main/java/com/resto/shop/web/report/OrderMapperReport.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/report/OrderMapperReport.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-public interface OrderMapper  extends GenericDao<Order,String> {
+public interface OrderMapperReport  extends GenericDao<Order,String> {
     int deleteByPrimaryKey(String id);
 
     int insert(Order record);
