a416e8851f24c1e8950f925a79dedf86d4250244
xiaonan <645534731@qq.com>
2018-01-17 18:13:16 +0800

    121

--- resto-shop-server/src/main/java/com/resto/shop/web/dao/OffLineOrderMapper.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/dao/OffLineOrderMapper.java
@@ -62,8 +62,6 @@ public interface OffLineOrderMapper  extends GenericDao<OffLineOrder,String> {
 
     OffLineOrder selectSumByTimeSourceAndShopId(@Param("orderType") int offlinePos, @Param("shopId") String id,@Param("beginDate") Date begin,@Param("endDate") Date end);
 
-    List<OrderNumDto> selectOrderNumByTimeAndBrandId(@Param("brandId") String brandId, @Param("beginDate") Date beginDate,@Param("endDate") Date endDate);
-
     /**
      * 结店 时 查询线下 日 和月的线下数据
      * @param todayBegin
