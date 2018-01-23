0431f37afca4eab6f568dd7be4e0302fbc989a90
xiaonan <645534731@qq.com>
2018-01-23 10:33:23 +0800

    121

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/ThirdServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/ThirdServiceImpl.java
@@ -192,6 +192,10 @@ public class ThirdServiceImpl implements ThirdService {
             return printTask;
         }
 
+        if(order.getProductionStatus() == 1){
+            return null;
+        }
+
         List<PlatformOrderDetail> orderDetailList = platformOrderDetailService.selectByPlatformOrderId(platformOrderId);
         List<PlatformOrderExtra> orderExtraList = platformOrderExtraService.selectByPlatformOrderId(platformOrderId);
 
