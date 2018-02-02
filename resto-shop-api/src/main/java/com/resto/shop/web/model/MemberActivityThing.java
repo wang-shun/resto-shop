f22b323f00edfabf0171a0bdb8f47ee8fd294dba
xiaonan <645534731@qq.com>
2018-02-02 10:31:21 +0800

    121

---
+++ resto-shop-api/src/main/java/com/resto/shop/web/model/MemberActivityThing.java
@@ -0,0 +1,47 @@
+package com.resto.shop.web.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class MemberActivityThing implements Serializable {
+
+    private Integer id;
+
+    private Date createTime;
+
+    private String telephone;
+
+    private Integer activityId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Integer getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(Integer activityId) {
+        this.activityId = activityId;
+    }
+}
---
+++ resto-shop-api/src/main/java/com/resto/shop/web/service/MemberActivityThingService.java
@@ -0,0 +1,7 @@
+package com.resto.shop.web.service;
+
+import com.resto.brand.core.generic.GenericService;
+import com.resto.shop.web.model.MemberActivityThing;
+
+public interface MemberActivityThingService extends GenericService<MemberActivityThing, Integer> {
+}
