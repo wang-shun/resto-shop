0346f82e418b289d8cd17a3dd6d5462680460a82
xiaonan <645534731@qq.com>
2018-02-05 11:10:32 +0800

    121

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
@@ -748,6 +748,9 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
                 return jsonResult;
             }
             item.setRemark(remark);
+            if(order.getMemberDiscount() != null && order.getMemberDiscount().doubleValue() > 0){
+                fans_price = fans_price.multiply(order.getMemberDiscount());
+            }
             if (fans_price != null && "pos".equals(order.getCreateOrderByAddress()) && shopDetail.getPosPlusType() == 1) {
                 item.setUnitPrice(price);
             } else if (fans_price != null && "pos".equals(order.getCreateOrderByAddress()) && shopDetail.getPosPlusType() != 1) {
@@ -1009,8 +1012,8 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
             order.setSerialNumber(DateFormatUtils.format(new Date(), "yyyyMMddHHmmssSSSS")); // 流水号
             order.setOriginalAmount(originMoney.add(order.getServicePrice()).add(order.getMealFeePrice()).add(extraMoney));// 原价
             order.setReductionAmount(BigDecimal.ZERO);// 折扣金额
-            order.setOrderMoney(totalMoney.subtract(order.getMemberDiscountMoney()).add(order.getServicePrice()).add(order.getMealFeePrice())); // 订单实际金额
-            order.setPaymentAmount(payMoney.subtract(order.getMemberDiscountMoney())); // 订单剩余需要维修支付的金额
+            order.setOrderMoney(totalMoney.add(order.getServicePrice()).add(order.getMealFeePrice())); // 订单实际金额
+            order.setPaymentAmount(payMoney); // 订单剩余需要维修支付的金额
             order.setPrintTimes(0);
             //判断当前订单所在店铺的服务费类型  0：经典版  1：升级版
             if (Common.YES.equals(shopDetail.getServiceType())){ //如果是启用的升级版服务费则将其拆分(拆分为shop端店铺设置开启的餐具费、纸巾费、酱料费)
