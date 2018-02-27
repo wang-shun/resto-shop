e1e25bff15bde7c21a7fbe79879deb0cca744d77
xiaonan <645534731@qq.com>
2018-02-27 20:16:59 +0800

    121

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/OrderServiceImpl.java
@@ -4598,7 +4598,6 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
         Integer orginState = order.getOrderState();//订单开始确认的状体
         if (order.getConfirmTime() == null && !order.getClosed()) {
             order.setOrderState(OrderState.CONFIRM);
-            log.error("22222222222222222222");
             if(order.getProductionStatus() == ProductionStatus.HAS_ORDER){
                 order.setProductionStatus(ProductionStatus.PRINTED);
             }
@@ -4679,7 +4678,6 @@ public class OrderServiceImpl extends GenericServiceImpl<Order, String> implemen
         log.info("开始确认订单:" + order.getId());
         if (order.getConfirmTime() == null && !order.getClosed()) {
             order.setOrderState(OrderState.CONFIRM);
-            log.error("11111111111111111111111111");
             if(order.getProductionStatus() == ProductionStatus.HAS_ORDER){
                 order.setProductionStatus(ProductionStatus.PRINTED);
             }
