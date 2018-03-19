50542dfba0998ec36ff5f5b7ff4d8f11b0c511e1
xiaonan <645534731@qq.com>
2018-03-19 16:21:10 +0800

    121

--- resto-shop-server/src/main/java/com/resto/shop/web/dao/WeightPackageDetailMapper.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/dao/WeightPackageDetailMapper.java
@@ -4,4 +4,17 @@ import com.resto.brand.core.generic.GenericDao;
 import com.resto.shop.web.model.WeightPackageDetail;
 
 public interface WeightPackageDetailMapper extends GenericDao<WeightPackageDetail,Long> {
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(WeightPackageDetail record);
+
+    int insertSelective(WeightPackageDetail record);
+
+    WeightPackageDetail selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(WeightPackageDetail record);
+
+    int updateByPrimaryKey(WeightPackageDetail record);
+
 }
--- resto-shop-server/src/main/java/com/resto/shop/web/dao/WeightPackageMapper.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/dao/WeightPackageMapper.java
@@ -4,4 +4,17 @@ import com.resto.brand.core.generic.GenericDao;
 import com.resto.shop.web.model.WeightPackage;
 
 public interface WeightPackageMapper extends GenericDao<WeightPackage,Long> {
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(WeightPackage record);
+
+    int insertSelective(WeightPackage record);
+
+    WeightPackage selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(WeightPackage record);
+
+    int updateByPrimaryKey(WeightPackage record);
+
 }
