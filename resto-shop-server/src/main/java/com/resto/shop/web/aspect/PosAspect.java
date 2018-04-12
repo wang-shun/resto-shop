91308666816a35040f84d5f110716ac9b019ce50
wutinglong <ting255047>
2018-04-12 10:45:38 +0800

    update

--- resto-shop-server/src/main/java/com/resto/shop/web/aspect/PosAspect.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/aspect/PosAspect.java
@@ -65,52 +65,52 @@ public class PosAspect {
     @AfterReturning(value = "updateData()", returning = "resultInfo")
     public void updateData(JoinPoint point, String resultInfo){
         log.info("修改数据完成，进入切面返回信息：" + resultInfo);
-//        //得到调用此方法时传递的参数
-//        String paramData = (String) point.getArgs()[0];
-//        JSONObject param = new JSONObject(paramData);
-//        //得到此次修改数据的业务类型
-//        String serverType = param.getString("serverType");
-//        JSONObject returnObject = new JSONObject(resultInfo);
-//        List<String> orderIds = (List<String>) returnObject.get("orderIds");
-//        Order order = new Order();
-//        //判断业务类型进行联动操作
-//        switch (serverType){
-//            case "weightPackage":
-//                log.info("开始修改重量包后的后续操作");
-//                //修改重量包
-//                List<String> orderItemIds = (List<String>) returnObject.get("orderItemIds");
-//                //得到当前订单
-//                order = orderService.selectById(orderIds.get(0));
-//                //查询到当前用户
-//                Customer customer = customerService.selectById(order.getCustomerId());
-//                if (customer != null){
-//                    String sendMessage = "报告老板，商家已确认您的订单，香喷喷的美食马上就来~\n" +
-//                            "菜品明细：\n";
-//                    for (String orderItemId : orderItemIds){
-//                        OrderItem orderItem = orderItemService.selectById(orderItemId);
-//                        sendMessage = sendMessage + orderItem.getArticleName() + "  ×1";
-//                    }
-//                    WechatConfig wechatConfig = wechatConfigService.selectByBrandId(order.getBrandId());
-//                    log.info("发送微信消息：" + sendMessage);
-//                    WeChatUtils.sendCustomerMsg(sendMessage, customer.getWechatId(), wechatConfig.getAppid(), wechatConfig.getAppsecret());
-//                }
-//                break;
-//            case "orderPay":
-//                log.info("开始支付订单后的后续操作");
-//                for (String orderId : orderIds){
-//                    order = orderService.selectById(orderId);
-//                    if (StringUtils.isBlank(order.getParentOrderId())){
-//                        break;
-//                    }
-//                }
-//                //释放桌位
-//                RedisUtil.set(order.getShopDetailId()+order.getTableNumber()+"status",true);
-//                //确认订单
-//                orderService.confirmBossOrder(order);
-//                break;
-//            default:
-//                log.info("本次调用无后续联动操作");
-//                break;
-//        }
+        //得到调用此方法时传递的参数
+        String paramData = (String) point.getArgs()[0];
+        JSONObject param = new JSONObject(paramData);
+        //得到此次修改数据的业务类型
+        String serverType = param.getString("serverType");
+        JSONObject returnObject = new JSONObject(resultInfo);
+        List<String> orderIds = (List<String>) returnObject.get("orderIds");
+        Order order = new Order();
+        //判断业务类型进行联动操作
+        switch (serverType){
+            case "weightPackage":
+                log.info("开始修改重量包后的后续操作");
+                //修改重量包
+                List<String> orderItemIds = (List<String>) returnObject.get("orderItemIds");
+                //得到当前订单
+                order = orderService.selectById(orderIds.get(0));
+                //查询到当前用户
+                Customer customer = customerService.selectById(order.getCustomerId());
+                if (customer != null){
+                    String sendMessage = "报告老板，商家已确认您的订单，香喷喷的美食马上就来~\n" +
+                            "菜品明细：\n";
+                    for (String orderItemId : orderItemIds){
+                        OrderItem orderItem = orderItemService.selectById(orderItemId);
+                        sendMessage = sendMessage + orderItem.getArticleName() + "  ×1";
+                    }
+                    WechatConfig wechatConfig = wechatConfigService.selectByBrandId(order.getBrandId());
+                    log.info("发送微信消息：" + sendMessage);
+                    WeChatUtils.sendCustomerMsg(sendMessage, customer.getWechatId(), wechatConfig.getAppid(), wechatConfig.getAppsecret());
+                }
+                break;
+            case "orderPay":
+                log.info("开始支付订单后的后续操作");
+                for (String orderId : orderIds){
+                    order = orderService.selectById(orderId);
+                    if (StringUtils.isBlank(order.getParentOrderId())){
+                        break;
+                    }
+                }
+                //释放桌位
+                RedisUtil.set(order.getShopDetailId()+order.getTableNumber()+"status",true);
+                //确认订单
+                orderService.confirmBossOrder(order);
+                break;
+            default:
+                log.info("本次调用无后续联动操作");
+                break;
+        }
     }
 }
