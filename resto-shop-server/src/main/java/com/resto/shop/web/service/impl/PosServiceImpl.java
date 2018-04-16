e699773c52ad0c015442400aff72b4b42151fbca
limingxin <906529775@qq.com>
2018-04-16 18:53:40 +0800

    修复了newPos编辑服务费报错的bug

--- resto-shop-server/src/main/java/com/resto/shop/web/service/impl/PosServiceImpl.java
+++ resto-shop-server/src/main/java/com/resto/shop/web/service/impl/PosServiceImpl.java
@@ -565,13 +565,17 @@ public class PosServiceImpl implements PosService {
             if (refundOrder.getOrderState() == OrderState.SUBMIT) {
                 for (OrderItem orderItem : order.getOrderItems()) {
                     OrderItem item = orderItemService.selectById(orderItem.getId());
-                    if (orderItem.getType() != ArticleType.ARTICLE) {
-                        //此处退的是服务费
-                        item = new OrderItem();
-                        item.setOrderId(orderItem.getOrderId());
+//                    if (orderItem.getType() != ArticleType.ARTICLE) {
+//                        //此处退的是服务费
+//                        item = new OrderItem();
+//                        item.setOrderId(orderItem.getOrderId());
+//                        item.setCount(order.getCustomerCount());
+//                    }
+                    //  如果退的是老版服务费
+                    if(orderItem.getType() == ArticleType.SERVICE_PRICE){
                         item.setCount(order.getCustomerCount());
                     }
-                    orderService.updateOrderItem(item.getOrderId(), item.getCount() - orderItem.getCount(), orderItem.getId(), 1);
+                    orderService.updateOrderItem(item.getOrderId(), item.getCount() - orderItem.getCount(), orderItem.getId(), orderItem.getType());
                 }
             } else {
                 orderService.refundItem(order);
